1d8725fcff808d584c01ce82498a2e6793060101
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-07-22 16:40:53 +0800

    add: auth bloc using streams

---
+++ lib/auth/bloc/auth_event.dart
@@ -0,0 +1,9 @@
+part of 'auth_bloc.dart';
+
+@immutable
+sealed class AuthEvent {}
+
+final class AuthStatusChanged extends AuthEvent {
+  final AuthenticationStatus status;
+  AuthStatusChanged(this.status);
+}
---
+++ lib/auth/bloc/auth_state.dart
@@ -0,0 +1,30 @@
+part of 'auth_bloc.dart';
+
+@immutable
+sealed class AuthState extends Equatable {
+  const AuthState({
+    this.status = AuthenticationStatus.unknown,
+    this.user = const User.empty(),
+  });
+
+  final AuthenticationStatus status;
+  final User user;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [status, user];
+}
+
+final class _AuthInitial extends AuthState {
+  const _AuthInitial() : super();
+}
+
+final class _Authenticated extends AuthState {
+  const _Authenticated(User user)
+      : super(status: AuthenticationStatus.authenticated, user: user);
+}
+
+final class _Unauthenticated extends AuthState {
+  const _Unauthenticated()
+      : super(status: AuthenticationStatus.unauthenticated);
+}
