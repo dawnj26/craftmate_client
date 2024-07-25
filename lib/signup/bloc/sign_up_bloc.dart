d7aa289d1fbb29548cf11136eca121ecf91f23ae
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-07-25 11:36:20 +0800

    add: sign up bloc

--- lib/signup/bloc/sign_up_bloc.dart
+++ lib/signup/bloc/sign_up_bloc.dart
@@ -1,13 +1,163 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
+import 'package:craftmate_client/logger.dart';
+import 'package:craftmate_client/login/models/email.dart';
+import 'package:craftmate_client/signup/models/models.dart';
+import 'package:equatable/equatable.dart';
+import 'package:formz/formz.dart';
 import 'package:meta/meta.dart';
 
 part 'sign_up_event.dart';
 part 'sign_up_state.dart';
 
 class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
-  SignUpBloc() : super(SignUpInitial()) {
-    on<SignUpEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  SignUpBloc({required AuthenticationRepository authenticationRepository})
+      : _authRepo = authenticationRepository,
+        super(
+          const SignUpInitial(),
+        ) {
+    on<SignUpNameChanged>(_onNameChanged);
+    on<SignUpEmailChanged>(_onEmailChanged);
+    on<SignUpPasswordChanged>(_onPasswordChanged);
+    on<SignUpSubmitted>(_onSubmit);
+    on<SignUpConfirmPasswordChanged>(_onConfirmPasswordChange);
+  }
+
+  final AuthenticationRepository _authRepo;
+
+  void _onNameChanged(SignUpNameChanged event, Emitter<SignUpState> emit) {
+    final name = Name.dirty(event.name);
+
+    emit(
+      SignUpInitial(
+        name: name,
+        email: state.email,
+        password: state.password,
+        confirmPassword: state.confirmPassword,
+        isValid: Formz.validate(
+          [name, state.email, state.password],
+        ),
+      ),
+    );
+  }
+
+  void _onEmailChanged(SignUpEmailChanged event, Emitter<SignUpState> emit) {
+    final email = Email.dirty(event.email);
+
+    emit(
+      SignUpInitial(
+        name: state.name,
+        email: email,
+        password: state.password,
+        confirmPassword: state.confirmPassword,
+        isValid: Formz.validate(
+          [state.name, email, state.password],
+        ),
+      ),
+    );
+  }
+
+  void _onPasswordChanged(
+    SignUpPasswordChanged event,
+    Emitter<SignUpState> emit,
+  ) {
+    final password = Password.dirty(event.password);
+
+    emit(
+      SignUpInitial(
+        name: state.name,
+        email: state.email,
+        password: password,
+        confirmPassword: state.confirmPassword,
+        isValid: Formz.validate(
+          [state.name, state.email, password],
+        ),
+      ),
+    );
+  }
+
+  void _onConfirmPasswordChange(
+    SignUpConfirmPasswordChanged event,
+    Emitter<SignUpState> emit,
+  ) {
+    final confirmPassword = ConfirmPassword.dirty(
+      password: state.password.value,
+      value: event.confirmPassword,
+    );
+
+    emit(
+      SignUpInitial(
+        name: state.name,
+        email: state.email,
+        password: state.password,
+        confirmPassword: confirmPassword,
+        isValid: Formz.validate(
+          [state.name, state.email, state.password, confirmPassword],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _onSubmit(
+      SignUpSubmitted event, Emitter<SignUpState> emit) async {
+    if (state.isValid) {
+      emit(
+        SignUpInProgress(
+          isValid: state.isValid,
+          email: state.email,
+          name: state.name,
+          password: state.password,
+          confirmPassword: state.confirmPassword,
+        ),
+      );
+
+      try {
+        await _authRepo.signUpWithEmailAndPassword(
+          name: state.name.value,
+          email: state.email.value,
+          password: state.password.value,
+        );
+        emit(
+          SignUpSuccess(
+            isValid: state.isValid,
+            email: state.email,
+            name: state.name,
+            password: state.password,
+            confirmPassword: state.confirmPassword,
+          ),
+        );
+      } on AuthException catch (e) {
+        log.e(e);
+        emit(
+          SignUpFailed(
+            message: e.message,
+            isValid: state.isValid,
+            email: state.email,
+            name: state.name,
+            password: state.password,
+            confirmPassword: state.confirmPassword,
+          ),
+        );
+      }
+    } else {
+      final name = Name.dirty(event.name);
+      final email = Email.dirty(event.email);
+      final password = Password.dirty(event.password);
+      final confirmPassword = ConfirmPassword.dirty(
+        password: event.password,
+        value: event.confirmPassword,
+      );
+      emit(
+        SignUpInitial(
+          name: name,
+          email: email,
+          password: password,
+          confirmPassword: confirmPassword,
+          isValid: Formz.validate(
+            [name, email, password, confirmPassword],
+          ),
+        ),
+      );
+    }
   }
 }
