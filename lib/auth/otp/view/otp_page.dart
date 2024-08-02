c8fbbde359e69db451d7617b199d59e5d8da7ae3
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-02 23:44:21 +0800

    add: email as param to able to resend otp

--- lib/auth/otp/view/otp_page.dart
+++ lib/auth/otp/view/otp_page.dart
@@ -1,18 +1,43 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:craftmate_client/auth/components/page_transitions.dart';
+import 'package:craftmate_client/auth/otp/bloc/otp_bloc.dart';
+import 'package:craftmate_client/auth/otp/timer/bloc/timer_bloc.dart';
+import 'package:craftmate_client/auth/otp/timer/models/ticker.dart';
 import 'package:craftmate_client/auth/otp/view/otp_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OtpPage extends StatelessWidget {
-  const OtpPage({super.key});
+  const OtpPage({required this.email, super.key});
 
-  static Route<void> route() {
-    return slideFromRightToLeft(const OtpPage());
+  final String email;
+
+  static Route<void> route(String email) {
+    return slideFromRightToLeft(
+      OtpPage(
+        email: email,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: OtpForm(),
+    return Scaffold(
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) => OtpBloc(
+              email: email,
+              authRepo:
+                  RepositoryProvider.of<AuthenticationRepository>(context),
+            ),
+          ),
+          BlocProvider(
+            create: (_) => TimerBloc(ticker: const Ticker()),
+          ),
+        ],
+        child: const OtpForm(),
+      ),
     );
   }
 }
