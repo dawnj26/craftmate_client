7786c517ae0e40833eadaa9130456b90e6f94aad
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-12 21:00:03 +0800

    refactor: use transition helper

--- lib/auth/reset_password/view/reset_password_page.dart
+++ lib/auth/reset_password/view/reset_password_page.dart
@@ -1,7 +1,7 @@
 import 'package:authentication_repository/authentication_repository.dart';
-import 'package:craftmate_client/auth/components/page_transitions.dart';
 import 'package:craftmate_client/auth/reset_password/bloc/reset_password_bloc.dart';
 import 'package:craftmate_client/auth/reset_password/view/reset_password_form.dart';
+import 'package:craftmate_client/helpers/transition/page_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,7 +11,7 @@ class ResetPasswordPage extends StatelessWidget {
   final String token;
 
   static Route<void> route(String token) {
-    return slideFromRightToLeft(
+    return PageTransition.effect.slideFromRightToLeft(
       ResetPasswordPage(
         token: token,
       ),
--- lib/auth/signup/view/signup_page.dart
+++ lib/auth/signup/view/signup_page.dart
@@ -1,7 +1,7 @@
 import 'package:authentication_repository/authentication_repository.dart';
-import 'package:craftmate_client/auth/components/components.dart';
 import 'package:craftmate_client/auth/signup/bloc/sign_up_bloc.dart';
 import 'package:craftmate_client/auth/signup/view/signup_form.dart';
+import 'package:craftmate_client/helpers/transition/page_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,7 +9,7 @@ class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
 
   static Route<void> route() {
-    return slideFromRightToLeft(const SignUpPage());
+    return PageTransition.effect.slideFromRightToLeft(const SignUpPage());
   }
 
   @override
