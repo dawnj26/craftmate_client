f35fc6f18e2d434b68dd64543deb1a7c8da1743c
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-19 21:25:49 +0800

    add: bloc provider for blank project

--- lib/project_management/blank_project/view/blank_project_page.dart
+++ lib/project_management/blank_project/view/blank_project_page.dart
@@ -1,6 +1,6 @@
 import 'package:craftmate_client/helpers/transition/page_transition.dart';
-import 'package:craftmate_client/project_management/blank_project/view/blank_project_screen.dart';
-import 'package:craftmate_client/project_management/cubits/step_title/cubit/step_title_cubit.dart';
+import 'package:craftmate_client/project_management/blank_project/bloc/blank_project_bloc.dart';
+import 'package:craftmate_client/project_management/blank_project/view/create/create_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,8 +14,8 @@ class BlankProjectPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => StepTitleCubit(),
-      child: const BlankProjectScreen(),
+      create: (context) => BlankProjectBloc(),
+      child: const BlankProjectCreateScreen(),
     );
   }
 }
