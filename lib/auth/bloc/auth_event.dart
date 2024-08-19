9b58b0fbee5b033182677ebb5a447b93ba376fa6
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-19 21:23:15 +0800

    fix: make auth bloc event use Equatable

--- lib/auth/bloc/auth_event.dart
+++ lib/auth/bloc/auth_event.dart
@@ -1,7 +1,11 @@
 part of 'auth_bloc.dart';
 
 @immutable
-sealed class AuthEvent {}
+sealed class AuthEvent extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
 final class AuthStatusChanged extends AuthEvent {
   final AuthenticationStatus status;
