0b4e00f83dcfa86841ec9a5282015899f847e120
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-20 18:11:47 +0800

    add(blank_project): listen to states and respond to it

--- lib/project_management/blank_project/view/create/create_screen.dart
+++ lib/project_management/blank_project/view/create/create_screen.dart
@@ -9,22 +9,65 @@ class BlankProjectCreateScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Blank project'),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.only(
-          left: 12.0,
-          right: 12.0,
-          bottom: 12.0,
+    return BlocListener<BlankProjectBloc, BlankProjectState>(
+      listener: (context, state) {
+        if (state is BlankProjectInProgress) {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (_) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          );
+        }
+
+        if (state is BlankProjectFailed) {
+          Navigator.of(context).pop();
+
+          final msg = SnackBar(
+            content: Text(
+              state.errorMsg,
+            ),
+          );
+
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(msg);
+        }
+
+        if (state is BlankProjectSuccess) {
+          Navigator.of(context).pop();
+
+          const msg = SnackBar(
+            content: Text(
+              'Created successfully',
+            ),
+          );
+
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(msg);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Blank project'),
         ),
-        child: Column(
-          children: [
-            _Switch(),
-            Gap(4.0),
-            _Form(),
-          ],
+        body: const Padding(
+          padding: EdgeInsets.only(
+            left: 12.0,
+            right: 12.0,
+            bottom: 12.0,
+          ),
+          child: Column(
+            children: [
+              _Switch(),
+              Gap(4.0),
+              _Form(),
+            ],
+          ),
         ),
       ),
     );
@@ -66,12 +109,16 @@ class _Form extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tagController = TextEditingController();
+    final tagFocusNode = FocusNode();
+    final titleFocusNode = FocusNode();
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         BlocBuilder<BlankProjectBloc, BlankProjectState>(
           buildWhen: (previous, current) {
-            return previous.projectTitle.value != current.projectTitle.value;
+            return previous.projectTitle != current.projectTitle;
           },
           builder: (context, state) {
             String? errorText;
@@ -83,6 +130,7 @@ class _Form extends StatelessWidget {
             }
 
             return _CustomTextField(
+              focusNode: titleFocusNode,
               key: const Key('projectTitle_textField_blankProject'),
               icon: const Icon(Icons.text_fields),
               labelText: 'Project title',
@@ -95,56 +143,36 @@ class _Form extends StatelessWidget {
           },
         ),
         const Gap(12.0),
-        Builder(
-          builder: (_) => buildTextField(
-            icon: const Icon(Icons.description),
-            label: 'Description (optional)',
-            multiline: true,
-          ),
-        ),
-        const Gap(12.0),
-        Builder(
-          builder: (_) => buildTextField(
-            icon: const Icon(Icons.category),
-            label: 'Categories/Tags',
-            helperText: 'Separated by whitespaces',
-          ),
+        _CustomTextField(
+          controller: tagController,
+          labelText: 'Categories/Tags (optional)',
+          multiline: false,
+          icon: const Icon(Icons.category),
+          helperText: 'Separated by whitespaces',
+          focusNode: tagFocusNode,
         ),
         const Gap(40.0),
         FilledButton(
-          onPressed: () {},
+          onPressed: () {
+            String? tags;
+            if (tagController.text.isNotEmpty) {
+              tags = tagController.text;
+            }
+
+            context.read<BlankProjectBloc>().add(
+                  BlankProjectCreate(tags: tags),
+                );
+          },
           child: const Text('Create Project'),
         ),
       ],
-    );
-  }
-
-  Widget buildTextField({
-    required String label,
-    bool multiline = false,
-    required Icon icon,
-    String? helperText,
-    String? errorText,
-    void Function(String)? onChanged,
-    Key? key,
-  }) {
-    final focusNode = FocusNode();
-
-    return _CustomTextField(
-      key: key,
-      helperText: helperText,
-      icon: icon,
-      focusNode: focusNode,
-      labelText: label,
-      multiline: multiline,
-      errorText: errorText,
-      onChanged: onChanged,
     );
   }
 }
 
 class _CustomTextField extends StatelessWidget {
   const _CustomTextField({
+    this.controller,
     super.key,
     this.onChanged,
     required this.labelText,
@@ -162,10 +190,12 @@ class _CustomTextField extends StatelessWidget {
   final String? helperText;
   final String? errorText;
   final void Function(String)? onChanged;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       focusNode: focusNode,
       decoration: InputDecoration(
         filled: true,
