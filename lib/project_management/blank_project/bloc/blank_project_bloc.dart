f58bfee94221f44da8547b1fa45918d3ac695366
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-20 18:11:17 +0800

    add(blank_project): project repository and onCreate logic

--- lib/project_management/blank_project/bloc/blank_project_bloc.dart
+++ lib/project_management/blank_project/bloc/blank_project_bloc.dart
@@ -2,15 +2,21 @@ import 'package:bloc/bloc.dart';
 import 'package:craftmate_client/project_management/blank_project/models/title.dart';
 import 'package:equatable/equatable.dart';
 import 'package:formz/formz.dart';
+import 'package:project_repository/project_repository.dart';
 
 part 'blank_project_event.dart';
 part 'blank_project_state.dart';
 
 class BlankProjectBloc extends Bloc<BlankProjectEvent, BlankProjectState> {
-  BlankProjectBloc() : super(const BlankProjectInitial()) {
+  BlankProjectBloc({required ProjectRepository projectRepo})
+      : _projectRepo = projectRepo,
+        super(const BlankProjectInitial()) {
     on<BlankProjectTitleChange>(_onTitleChange);
     on<BlankProjectToggleVisibility>(_onToggleVisibility);
+    on<BlankProjectCreate>(_onCreate);
   }
+
+  final ProjectRepository _projectRepo;
 
   void _onTitleChange(
     BlankProjectTitleChange event,
@@ -39,5 +45,55 @@ class BlankProjectBloc extends Bloc<BlankProjectEvent, BlankProjectState> {
         valid: state.valid,
       ),
     );
+  }
+
+  Future<void> _onCreate(
+    BlankProjectCreate event,
+    Emitter<BlankProjectState> emit,
+  ) async {
+    if (state.valid) {
+      emit(
+        BlankProjectInProgress(
+          projectTitle: state.projectTitle,
+          valid: state.valid,
+          isPulic: state.isPulic,
+        ),
+      );
+
+      try {
+        await _projectRepo.tryCreateProject(
+          state.projectTitle.value,
+          state.isPulic,
+          event.tags,
+        );
+        emit(
+          BlankProjectSuccess(
+            isPulic: state.isPulic,
+            projectTitle: state.projectTitle,
+            valid: state.valid,
+          ),
+        );
+      } on ProjectException catch (e) {
+        emit(
+          BlankProjectFailed(
+            errorMsg: e.message,
+            projectTitle: state.projectTitle,
+            valid: state.valid,
+            isPulic: state.isPulic,
+          ),
+        );
+      }
+    } else {
+      final projectTitle = ProjectTitle.dirty(state.projectTitle.value);
+      final isValid = Formz.validate([projectTitle]);
+
+      emit(
+        BlankProjectInvalid(
+          projectTitle: projectTitle,
+          valid: isValid,
+          isPulic: state.isPulic,
+        ),
+      );
+    }
   }
 }
