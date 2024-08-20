ce5348dba52cd3e374100d1d33cb004ebc537719
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-20 18:12:03 +0800

    add(blank_project): project repository

--- lib/project_management/blank_project/view/blank_project_page.dart
+++ lib/project_management/blank_project/view/blank_project_page.dart
@@ -1,8 +1,10 @@
+import 'package:craftmate_client/globals.dart';
 import 'package:craftmate_client/helpers/transition/page_transition.dart';
 import 'package:craftmate_client/project_management/blank_project/bloc/blank_project_bloc.dart';
 import 'package:craftmate_client/project_management/blank_project/view/create/create_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_repository/project_repository.dart';
 
 class BlankProjectPage extends StatelessWidget {
   const BlankProjectPage({super.key});
@@ -13,9 +15,14 @@ class BlankProjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => BlankProjectBloc(),
-      child: const BlankProjectCreateScreen(),
+    return RepositoryProvider(
+      create: (context) => ProjectRepository(config: config),
+      child: BlocProvider(
+        create: (context) => BlankProjectBloc(
+          projectRepo: RepositoryProvider.of<ProjectRepository>(context),
+        ),
+        child: const BlankProjectCreateScreen(),
+      ),
     );
   }
 }
