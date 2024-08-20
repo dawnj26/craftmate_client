8c37086c22b198ec63a82de6c2ca4442431a87d7
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-20 16:40:39 +0800

    add: flutter dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:craftmate_client/app.dart';
 import 'package:craftmate_client/logger.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:log_collector/log_collector.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   logger = await LogCollector.getInstance();
-  logger.logInfo('Log started');
 
+  logger.logInfo('Loading enviroment variables.');
+  await dotenv.load();
+
+  logger.logInfo('Starting the app');
   runApp(const MyApp());
 }
