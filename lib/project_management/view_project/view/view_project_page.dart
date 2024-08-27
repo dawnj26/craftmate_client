49ee324677d22b1ea85a46349395ec8cc4b7491d
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-27 21:02:27 +0800

    add(view_project): project model and replace it with its properties

--- lib/project_management/view_project/view/view_project_page.dart
+++ lib/project_management/view_project/view/view_project_page.dart
@@ -1,12 +1,23 @@
 import 'package:craftmate_client/helpers/transition/page_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:project_repository/project_repository.dart';
+import 'package:user_repository/user_repository.dart';
 
 class ViewProjectPage extends StatelessWidget {
-  const ViewProjectPage({super.key});
+  const ViewProjectPage({
+    super.key,
+    required this.project,
+  });
 
-  static Route<void> route() {
-    return PageTransition.effect.slideFromRightToLeft(const ViewProjectPage());
+  final Project project;
+
+  static Route<void> route(Project project) {
+    return PageTransition.effect.slideFromRightToLeft(
+      ViewProjectPage(
+        project: project,
+      ),
+    );
   }
 
   @override
@@ -27,7 +38,11 @@ class ViewProjectPage extends StatelessWidget {
       body: ListView(
         children: [
           Image.asset('assets/images/placeholder.png'),
-          const _ProjectCardHeader(),
+          _ProjectCardHeader(
+            creator: project.creator,
+            projectTitle: project.title,
+            tags: project.tags,
+          ),
         ],
       ),
     );
@@ -35,7 +50,15 @@ class ViewProjectPage extends StatelessWidget {
 }
 
 class _ProjectCardHeader extends StatelessWidget {
-  const _ProjectCardHeader();
+  const _ProjectCardHeader({
+    required this.creator,
+    required this.projectTitle,
+    this.tags,
+  });
+
+  final User creator;
+  final String projectTitle;
+  final List<Tag>? tags;
 
   @override
   Widget build(BuildContext context) {
@@ -48,30 +71,16 @@ class _ProjectCardHeader extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              const CircleAvatar(
-                radius: 16.0,
-                child: Text('A'),
-              ),
-              const Gap(12.0),
-              Text(
-                'John doe',
-                style: theme.textTheme.bodyMedium,
-              ),
-            ],
+          _Creator(
+            initialName: creator.name[0].toUpperCase(),
+            fullName: creator.name,
           ),
           const Gap(16.0),
-          const _ProjectInfo(
-            projectTitle: 'My favorite project',
-            projectDescription:
-                'No description No description No description No description No description No description No description No description No description No description No description No description',
+          _ProjectInfo(
+            projectTitle: projectTitle,
           ),
-          const Gap(16.0),
-          const Tags(),
-          const Gap(16.0),
+          Tags(tags: tags),
           const _SocialCounters(),
-          const Gap(8.0),
           const _ActionButtons(),
         ],
       ),
@@ -79,47 +88,56 @@ class _ProjectCardHeader extends StatelessWidget {
   }
 }
 
-class Tags extends StatelessWidget {
-  const Tags({super.key});
+class _Creator extends StatelessWidget {
+  const _Creator({
+    required this.initialName,
+    required this.fullName,
+  });
+
+  final String initialName;
+  final String fullName;
 
   @override
   Widget build(BuildContext context) {
-    return const Wrap(
-      spacing: 8.0,
-      runSpacing: 8.0,
+    final theme = Theme.of(context);
+    return Row(
       children: [
-        Tag(
-          tagText: 'paper',
+        CircleAvatar(
+          radius: 16.0,
+          child: Text(initialName),
         ),
-        Tag(
-          tagText: 'craziestshit',
-        ),
-        Tag(
-          tagText: 'paper',
-        ),
-        Tag(
-          tagText: 'craziestshit',
-        ),
-        Tag(
-          tagText: 'paper',
-        ),
-        Tag(
-          tagText: 'craziestshit',
-        ),
-        Tag(
-          tagText: 'craziestshit',
-        ),
-        Tag(
-          tagText: 'craziestshit',
+        const Gap(12.0),
+        Text(
+          fullName,
+          style: theme.textTheme.bodyMedium,
         ),
       ],
     );
   }
 }
 
-class Tag extends StatelessWidget {
-  const Tag({
-    super.key,
+class Tags extends StatelessWidget {
+  const Tags({super.key, required this.tags});
+
+  final List<Tag>? tags;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: tags == null || tags!.isEmpty
+          ? EdgeInsets.zero
+          : const EdgeInsets.symmetric(vertical: 16.0),
+      child: Wrap(
+        spacing: 8.0,
+        runSpacing: 8.0,
+        children: tags!.map((e) => _Tag(tagText: e.name)).toList(),
+      ),
+    );
+  }
+}
+
+class _Tag extends StatelessWidget {
+  const _Tag({
     required this.tagText,
   });
 
@@ -152,23 +170,26 @@ class _SocialCounters extends StatelessWidget {
   Widget build(BuildContext context) {
     const gap = 20.0;
 
-    return const Row(
-      children: [
-        _Counter(
-          countText: '12.1k',
-          icon: Icon(Icons.favorite_outline),
-        ),
-        Gap(gap),
-        _Counter(
-          countText: '24',
-          icon: Icon(Icons.transform),
-        ),
-        Gap(gap),
-        _Counter(
-          countText: '24',
-          icon: Icon(Icons.mode_comment_outlined),
-        ),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          _Counter(
+            countText: '12.1k',
+            icon: Icon(Icons.favorite_outline),
+          ),
+          Gap(gap),
+          _Counter(
+            countText: '24',
+            icon: Icon(Icons.transform),
+          ),
+          Gap(gap),
+          _Counter(
+            countText: '24',
+            icon: Icon(Icons.mode_comment_outlined),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -232,16 +253,13 @@ class _Counter extends StatelessWidget {
 class _ProjectInfo extends StatelessWidget {
   const _ProjectInfo({
     required this.projectTitle,
-    required this.projectDescription,
   });
 
   final String projectTitle;
-  final String projectDescription;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
     final textTheme = theme.textTheme;
 
     return Column(
@@ -250,14 +268,6 @@ class _ProjectInfo extends StatelessWidget {
         Text(
           projectTitle,
           style: textTheme.titleLarge,
-        ),
-        const Gap(4.0),
-        Text(
-          projectDescription,
-          style: TextStyle(
-            fontSize: textTheme.bodyLarge!.fontSize,
-            color: colorScheme.onSurfaceVariant,
-          ),
         ),
       ],
     );
