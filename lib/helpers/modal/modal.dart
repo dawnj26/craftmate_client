2963e8889871cb81b76f472fe7bacc1a77942024
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-05 11:20:52 +0800

    add(modal): confirmation modal

--- lib/helpers/modal/modal.dart
+++ lib/helpers/modal/modal.dart
@@ -9,23 +9,50 @@ class Modal {
     showDialog(
       context: context,
       builder: (_) {
-        return const _NonDismissibleLoadingDialog();
+        return const _NonDismissibleDialog(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
       },
       barrierDismissible: false,
     );
   }
+
+  Future<bool> showConfirmationModal({
+    required BuildContext context,
+    required String message,
+    required String title,
+    List<Widget>? actions,
+  }) async {
+    return await showDialog<bool>(
+          context: context,
+          builder: (_) {
+            return _NonDismissibleDialog(
+              child: AlertDialog(
+                title: Text(title),
+                content: Text(message),
+                actions: actions,
+              ),
+            );
+          },
+        ) ??
+        false;
+  }
 }
 
-class _NonDismissibleLoadingDialog extends StatelessWidget {
-  const _NonDismissibleLoadingDialog();
+class _NonDismissibleDialog extends StatelessWidget {
+  const _NonDismissibleDialog({
+    required this.child,
+  });
+
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return const PopScope(
+    return PopScope(
       canPop: false,
-      child: Center(
-        child: CircularProgressIndicator(),
-      ),
+      child: child,
     );
   }
 }
