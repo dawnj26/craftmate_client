50688e23581d3bc0f30682863154f07391029a25
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-05 11:24:01 +0800

    refactor(editor): move editor to edit project and make controller as param

---
+++ lib/project_management/edit_project/view/editor/editor_toolbar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
+
+class EditorToolBar extends StatelessWidget {
+  const EditorToolBar({
+    super.key,
+    required QuillController editorController,
+  }) : _editorController = editorController;
+
+  final QuillController _editorController;
+
+  @override
+  Widget build(BuildContext context) {
+    return QuillToolbar(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Wrap(
+          children: [
+            QuillToolbarHistoryButton(
+              controller: _editorController,
+              isUndo: true,
+            ),
+            QuillToolbarToggleStyleButton(
+              controller: _editorController,
+              attribute: Attribute.bold,
+            ),
+            QuillToolbarToggleStyleButton(
+              controller: _editorController,
+              attribute: Attribute.italic,
+            ),
+            QuillToolbarToggleStyleButton(
+              controller: _editorController,
+              attribute: Attribute.underline,
+            ),
+            QuillToolbarSelectHeaderStyleDropdownButton(
+              controller: _editorController,
+            ),
+            const VerticalDivider(),
+            QuillToolbarColorButton(
+              controller: _editorController,
+              isBackground: false,
+            ),
+            QuillToolbarToggleStyleButton(
+              controller: _editorController,
+              attribute: Attribute.ul,
+            ),
+            QuillToolbarToggleStyleButton(
+              controller: _editorController,
+              attribute: Attribute.ol,
+            ),
+            const VerticalDivider(),
+            QuillToolbarIndentButton(
+              controller: _editorController,
+              isIncrease: true,
+            ),
+            QuillToolbarIndentButton(
+              controller: _editorController,
+              isIncrease: false,
+            ),
+            QuillToolbarImageButton(controller: _editorController),
+            QuillToolbarVideoButton(controller: _editorController),
+          ],
+        ),
+      ),
+    );
+  }
+}
