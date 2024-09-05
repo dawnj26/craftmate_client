56e194216bb2d4b90db87e5060c41817cafde16a
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-05 11:27:20 +0800

    refactor: move editor to edit project

--- lib/project_management/steps/view/edit_step_screen.dart
+++ lib/project_management/steps/view/edit_step_screen.dart
@@ -1,4 +1,5 @@
 import 'package:craftmate_client/project_management/cubits/step_title/cubit/step_title_cubit.dart';
+import 'package:craftmate_client/project_management/edit_project/view/editor/content_editor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_quill/flutter_quill.dart';
@@ -11,24 +12,24 @@ class EditStepScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: BlocBuilder<StepTitleCubit, StepTitleState>(
-          buildWhen: (previous, current) => previous.title != current.title,
-          builder: (context, state) {
-            return Text(state.title);
-          },
-        ),
-        actions: [
-          TextButton.icon(
-            onPressed: () => showChangeTitle(context),
-            label: const Text('Edit title'),
-            icon: const Icon(Icons.edit),
-          ),
-        ],
-      ),
-      body: const ContentEditor(),
-    );
+    return const Scaffold(
+        // appBar: AppBar(
+        //   title: BlocBuilder<StepTitleCubit, StepTitleState>(
+        //     buildWhen: (previous, current) => previous.title != current.title,
+        //     builder: (context, state) {
+        //       return Text(state.title);
+        //     },
+        //   ),
+        //   actions: [
+        //     TextButton.icon(
+        //       onPressed: () => showChangeTitle(context),
+        //       label: const Text('Edit title'),
+        //       icon: const Icon(Icons.edit),
+        //     ),
+        //   ],
+        // ),
+        // body: const ContentEditor(),
+        );
   }
 
   void showChangeTitle(BuildContext context) {
@@ -68,125 +69,6 @@ class EditStepScreen extends StatelessWidget {
           ],
         );
       },
-    );
-  }
-}
-
-class ContentEditor extends StatefulWidget {
-  const ContentEditor({super.key});
-
-  @override
-  State<ContentEditor> createState() => _ContentEditorState();
-}
-
-class _ContentEditorState extends State<ContentEditor> {
-  final _editorFocusNode = FocusNode();
-  late final QuillController _editorController;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _editorController =
-        QuillController.basic(editorFocusNode: _editorFocusNode);
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _editorController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: QuillEditor(
-            controller: _editorController,
-            configurations: QuillEditorConfigurations(
-              padding: const EdgeInsets.all(12.0),
-              placeholder: 'Create your content here',
-              embedBuilders: FlutterQuillEmbeds.editorBuilders(),
-            ),
-            focusNode: _editorFocusNode,
-            scrollController: ScrollController(),
-          ),
-        ),
-        EditorToolBar(editorController: _editorController),
-      ],
-    );
-  }
-}
-
-class EditorToolBar extends StatelessWidget {
-  const EditorToolBar({
-    super.key,
-    required QuillController editorController,
-  }) : _editorController = editorController;
-
-  final QuillController _editorController;
-
-  @override
-  Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    return Container(
-      decoration: BoxDecoration(
-        color: colorScheme.surfaceContainerHigh,
-      ),
-      child: QuillToolbar(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Wrap(
-            children: [
-              QuillToolbarHistoryButton(
-                controller: _editorController,
-                isUndo: true,
-              ),
-              QuillToolbarToggleStyleButton(
-                controller: _editorController,
-                attribute: Attribute.bold,
-              ),
-              QuillToolbarToggleStyleButton(
-                controller: _editorController,
-                attribute: Attribute.italic,
-              ),
-              QuillToolbarToggleStyleButton(
-                controller: _editorController,
-                attribute: Attribute.underline,
-              ),
-              QuillToolbarSelectHeaderStyleButtons(
-                controller: _editorController,
-              ),
-              const VerticalDivider(),
-              QuillToolbarColorButton(
-                controller: _editorController,
-                isBackground: false,
-              ),
-              QuillToolbarToggleStyleButton(
-                controller: _editorController,
-                attribute: Attribute.ul,
-              ),
-              QuillToolbarToggleStyleButton(
-                controller: _editorController,
-                attribute: Attribute.ol,
-              ),
-              const VerticalDivider(),
-              QuillToolbarIndentButton(
-                controller: _editorController,
-                isIncrease: true,
-              ),
-              QuillToolbarIndentButton(
-                controller: _editorController,
-                isIncrease: false,
-              ),
-              QuillToolbarImageButton(controller: _editorController),
-              QuillToolbarVideoButton(controller: _editorController),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
