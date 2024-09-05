6f5a2052149b0e8e544829609680358b36bc8a6f
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-05 11:24:24 +0800

    add(edit_project): edit project screen

--- lib/project_management/edit_project/view/screens/edit_project_screen.dart
+++ lib/project_management/edit_project/view/screens/edit_project_screen.dart
@@ -6,7 +6,34 @@ class EditProjectScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Edit project'),
+      ),
+      body: const EditProjectBody(),
+    );
+  }
+}
+
+class EditProjectBody extends StatelessWidget {
+  const EditProjectBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: [
+          Text(
+            'Description',
+            style: textTheme.headlineLarge,
+          ),
+        ],
+      ),
     );
   }
 }
