ff09c88d7c2d34624dc6632d9e140c84265eb241
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-08 12:40:19 +0800

    add(steps): quill editor for displaying contents

--- lib/project_management/view_project/view/components/project_steps.dart
+++ lib/project_management/view_project/view/components/project_steps.dart
@@ -1,19 +1,29 @@
-import 'package:craftmate_client/project_management/edit_project/view/edit_project_page.dart';
-import 'package:craftmate_client/project_management/view_project/view/screen/view_project_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 
 class ProjectSteps extends StatelessWidget {
-  const ProjectSteps({super.key});
+  const ProjectSteps({
+    super.key,
+    required this.stepJson,
+  });
+
+  final List<dynamic>? stepJson;
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        ProjectBodySection(
-          sectionName: 'Steps',
-          type: EditProjectType.steps,
-        ),
-      ],
+    final document = Document.fromJson(stepJson!);
+    final controller = QuillController(
+      document: document,
+      selection: const TextSelection.collapsed(offset: 0),
+      readOnly: true,
+    );
+
+    return QuillEditor.basic(
+      controller: controller,
+      configurations: const QuillEditorConfigurations(
+        showCursor: false,
+        enableInteractiveSelection: false,
+      ),
     );
   }
 }
