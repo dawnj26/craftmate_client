b696798a9035944b2fb76c640e4979019f9f3446
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-08 12:46:41 +0800

    refactor: remove unused imports

--- lib/project_management/editor/view/step_toolbar.dart
+++ lib/project_management/editor/view/step_toolbar.dart
@@ -1,4 +1,3 @@
-import 'package:craftmate_client/globals.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
