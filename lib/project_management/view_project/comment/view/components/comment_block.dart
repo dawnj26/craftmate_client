2690342af9bd438a6d97f1c54cafdddfb59e86a7
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-17 12:05:18 +0800

    refactor: remove unused import

--- lib/project_management/view_project/comment/view/components/comment_block.dart
+++ lib/project_management/view_project/comment/view/components/comment_block.dart
@@ -1,5 +1,4 @@
 import 'package:craftmate_client/auth/auth.dart';
-import 'package:craftmate_client/globals.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project_repository/project_repository.dart';
