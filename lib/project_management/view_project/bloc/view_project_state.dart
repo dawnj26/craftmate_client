b6d7004c983f3e3f610c383debbd682808d07f79
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-21 16:00:40 +0800

    add(view_project): loading and upload success state

--- lib/project_management/view_project/bloc/view_project_state.dart
+++ lib/project_management/view_project/bloc/view_project_state.dart
@@ -17,10 +17,29 @@ final class ViewProjectInitial extends ViewProjectState {
   });
 }
 
+final class ViewProjectLoading extends ViewProjectState {
+  const ViewProjectLoading({
+    required super.project,
+  });
+}
+
 final class ViewProjectDirty extends ViewProjectState {
   const ViewProjectDirty({
     required super.project,
   });
+}
+
+final class ViewProjectUploadSuccess extends ViewProjectState {
+  final bool isModalOpen;
+
+  const ViewProjectUploadSuccess({
+    required super.project,
+    this.isModalOpen = false,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [isModalOpen];
 }
 
 final class ViewProjectFailed extends ViewProjectState {
