8ff68126e4094d7eccfe820bcc1216bde25922ee
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-26 19:08:59 +0800

    add: paginated project

--- lib/dashboard/home/bloc/home_state.dart
+++ lib/dashboard/home/bloc/home_state.dart
@@ -5,27 +5,35 @@ enum HomeStatus { initial, loading, loaded, error }
 final class HomeState extends Equatable {
   const HomeState({
     this.status = HomeStatus.initial,
+    this.paginatedProjects = const Pagination.empty(),
     this.projects = const <Project>[],
     this.trendingProjects = const <Project>[],
     this.message = '',
+    this.projectReachedMax = false,
   });
 
   final HomeStatus status;
+  final Pagination<Project> paginatedProjects;
   final List<Project> projects;
   final List<Project> trendingProjects;
   final String message;
+  final bool projectReachedMax;
 
   HomeState copyWith({
     HomeStatus? status,
+    Pagination<Project>? paginatedProjects,
     List<Project>? projects,
     List<Project>? trendingProjects,
     String? message,
+    bool? projectReachedMax,
   }) {
     return HomeState(
       status: status ?? this.status,
+      paginatedProjects: paginatedProjects ?? this.paginatedProjects,
       projects: projects ?? this.projects,
       trendingProjects: trendingProjects ?? this.trendingProjects,
       message: message ?? this.message,
+      projectReachedMax: projectReachedMax ?? this.projectReachedMax,
     );
   }
 
