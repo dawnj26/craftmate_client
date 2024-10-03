dab8c7315cb1702185349ce7f9499d4314cb990d
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-03 10:54:33 +0800

    add: filter enums

---
+++ packages/project_repository/lib/src/models/filters.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+enum ProjectFilter {
+  all('All'),
+  public('Public'),
+  private('Private'),
+  followers('Followers');
+
+  const ProjectFilter(this.label);
+  final String label;
+}
+
+enum ProjectSort {
+  title('Title', Icons.sort_by_alpha),
+  lastModified('Last modified', Icons.calendar_today),
+  lastCreated('Last created', Icons.calendar_today),
+  likes('Likes', Icons.favorite_outline_rounded),
+  comments('Comments', Icons.comment_outlined),
+  ;
+
+  const ProjectSort(this.label, this.icon);
+  final String label;
+  final IconData icon;
+}
+
+enum SortOrder { asc, desc }
+
+extension ProjectSortExtension on ProjectSort {
+  String get value {
+    switch (this) {
+      case ProjectSort.title:
+        return 'title';
+      case ProjectSort.lastModified:
+        return 'updated_at';
+      case ProjectSort.lastCreated:
+        return 'created_at';
+      case ProjectSort.likes:
+        return 'like_count';
+      case ProjectSort.comments:
+        return 'comment_count';
+    }
+  }
+}
+
+extension SortOrderExtension on SortOrder {
+  String get value => this == SortOrder.asc ? 'asc' : 'desc';
+}
