a9abd23af5c58f5e979ad8286aba3ae02c941b36
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-08 22:13:55 +0800

    refactor: use recipe saved event

--- lib/project_management/edit_project/view/screens/edit_recipe_screen.dart
+++ lib/project_management/edit_project/view/screens/edit_recipe_screen.dart
@@ -66,3 +66,94 @@ class _EditRecipeScreenState extends State<EditRecipeScreen> {
       ),
     );
   }
+
+  void _editDescriptionListener(BuildContext context, EditProjectState state) {
+    final nav = Navigator.of(context);
+
+    if (state is EditProjectLoading) {
+      Modal.instance.showLoadingDialog(context);
+    } else if (state is EditProjectFailed) {
+      nav.pop();
+      Alert.instance.showSnackbar(context, state.errMessage);
+    } else if (state is EditProjectClean) {
+      // !Pop loading dialog
+      nav.pop();
+
+      if (state.shouldExit) {
+        nav.pop();
+        return;
+      }
+
+      Alert.instance.showSnackbar(context, 'Saved successfully');
+    }
+  }
+
+  void _handleSave() {
+    FocusScope.of(context).unfocus();
+    final textEditorBloc = context.read<TextEditorBloc>();
+
+    BlocProvider.of<EditProjectBloc>(context).add(
+      EditProjectRecipeSaved(
+        newDescription: textEditorBloc.state.descriptionController!,
+        newSteps: textEditorBloc.state.controllers,
+        currentProject: widget.project,
+        shouldExit: true,
+      ),
+    );
+  }
+
+  Future<void> _handlePop(bool didPop, Object? result) async {
+    if (didPop) {
+      return;
+    }
+    final bloc = BlocProvider.of<EditProjectBloc>(context);
+    final currentState = bloc.state;
+    final isProjectChanged =
+        currentState is EditProjectDirty || currentState is EditProjectFailed;
+
+    // unfocus any focused node
+    WidgetsBinding.instance.focusManager.primaryFocus?.unfocus();
+
+    if (isProjectChanged) {
+      final shouldSave = await Modal.instance.showConfirmationModal(
+        context: context,
+        content: const Text('Do you want to save your changes?'),
+        title: 'Save changes',
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text('Discard'),
+          ),
+          FilledButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      );
+
+      if (shouldSave == null) {
+        return;
+      }
+
+      if (mounted && shouldSave) {
+        final editorState = BlocProvider.of<TextEditorBloc>(context).state;
+        bloc.add(
+          EditProjectRecipeSaved(
+            newDescription: editorState.descriptionController!,
+            newSteps: editorState.controllers,
+            currentProject: widget.project,
+            shouldExit: true,
+          ),
+        );
+      } else if (mounted) {
+        Navigator.of(context).pop();
+      }
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+}
