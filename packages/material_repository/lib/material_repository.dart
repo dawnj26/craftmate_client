a36f9eb461e7eeb95f32dabcc65b6be0db5d8809
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-15 15:16:11 +0800

    export material repo api

--- packages/material_repository/lib/material_repository.dart
+++ packages/material_repository/lib/material_repository.dart
@@ -1,7 +1,5 @@
 library material_repository;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/exceptions/material_exception.dart';
+export 'src/material_repository_base.dart';
+export 'src/models/models.dart';
