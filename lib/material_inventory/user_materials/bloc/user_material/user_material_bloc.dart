7ad8f1229de4b44d5b79df1a49cf9276fa630b0e
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-20 15:47:59 +0800

    add: reload event and make states public for pattern matching

--- lib/material_inventory/user_materials/bloc/user_material/user_material_bloc.dart
+++ lib/material_inventory/user_materials/bloc/user_material/user_material_bloc.dart
@@ -2,19 +2,33 @@ import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:material_repository/material_repository.dart';
 
+part 'user_material_bloc.freezed.dart';
 part 'user_material_event.dart';
 part 'user_material_state.dart';
-part 'user_material_bloc.freezed.dart';
 
 class UserMaterialBloc extends Bloc<UserMaterialEvent, UserMaterialState> {
   UserMaterialBloc({
     required MaterialRepository materialRepository,
   })  : _materialRepository = materialRepository,
-        super(const _Initial()) {
+        super(const Initial()) {
     on<_Started>(_onStarted);
+    on<_Reload>(_onReload);
   }
 
   final MaterialRepository _materialRepository;
+
+  Future<void> _onReload(
+    _Reload event,
+    Emitter<UserMaterialState> emit,
+  ) async {
+    emit(const UserMaterialState.loading());
+    try {
+      final materials = await _materialRepository.getMaterials();
+      emit(UserMaterialState.loaded(materials: materials));
+    } on MaterialException catch (e) {
+      emit(UserMaterialState.error(message: e.message));
+    }
+  }
 
   Future<void> _onStarted(
     _Started event,
--- lib/material_inventory/user_materials/bloc/user_material/user_material_state.dart
+++ lib/material_inventory/user_materials/bloc/user_material/user_material_state.dart
@@ -4,15 +4,15 @@ part of 'user_material_bloc.dart';
 class UserMaterialState with _$UserMaterialState {
   const factory UserMaterialState.initial({
     @Default([]) List<Material> materials,
-  }) = _Initial;
+  }) = Initial;
   const factory UserMaterialState.loading({
     @Default([]) List<Material> materials,
-  }) = _Loading;
+  }) = Loading;
   const factory UserMaterialState.loaded({
     @Default([]) List<Material> materials,
-  }) = _Loaded;
+  }) = Loaded;
   const factory UserMaterialState.error({
     @Default([]) List<Material> materials,
     required String message,
-  }) = _Error;
+  }) = Error;
 }
