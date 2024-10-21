3f661c7eda33565423f0a1d35036597f8b84fe68
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-21 23:15:28 +0800

    add: filter category and sort, handle sort/filter changes, and delete multiple materials

--- lib/material_inventory/user_materials/bloc/user_material/user_material_state.dart
+++ lib/material_inventory/user_materials/bloc/user_material/user_material_state.dart
@@ -4,15 +4,45 @@ part of 'user_material_bloc.dart';
 class UserMaterialState with _$UserMaterialState {
   const factory UserMaterialState.initial({
     @Default([]) List<Material> materials,
+    @Default([]) List<MaterialCategory> categories,
+    @Default(MaterialSort.lastModified) MaterialSort sort,
+    @Default(SortOrder.desc) SortOrder order,
+    int? categoryId,
   }) = Initial;
   const factory UserMaterialState.loading({
     @Default([]) List<Material> materials,
+    @Default([]) List<MaterialCategory> categories,
+    @Default(MaterialSort.lastModified) MaterialSort sort,
+    @Default(SortOrder.desc) SortOrder order,
+    int? categoryId,
   }) = Loading;
   const factory UserMaterialState.loaded({
     @Default([]) List<Material> materials,
+    @Default([]) List<MaterialCategory> categories,
+    @Default(MaterialSort.lastModified) MaterialSort sort,
+    @Default(SortOrder.desc) SortOrder order,
+    int? categoryId,
   }) = Loaded;
   const factory UserMaterialState.error({
     @Default([]) List<Material> materials,
+    @Default([]) List<MaterialCategory> categories,
+    @Default(MaterialSort.lastModified) MaterialSort sort,
+    @Default(SortOrder.desc) SortOrder order,
+    int? categoryId,
     required String message,
   }) = Error;
+  const factory UserMaterialState.deleting({
+    @Default([]) List<Material> materials,
+    @Default([]) List<MaterialCategory> categories,
+    @Default(MaterialSort.lastModified) MaterialSort sort,
+    @Default(SortOrder.desc) SortOrder order,
+    int? categoryId,
+  }) = Deleting;
+  const factory UserMaterialState.deleted({
+    @Default([]) List<Material> materials,
+    @Default([]) List<MaterialCategory> categories,
+    @Default(MaterialSort.lastModified) MaterialSort sort,
+    @Default(SortOrder.desc) SortOrder order,
+    int? categoryId,
+  }) = Deleted;
 }
