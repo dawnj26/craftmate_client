48244f7b6b697c37e20cc4960182c64ed90c7a2a
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-23 14:28:12 +0800

    export project fork

--- packages/project_repository/lib/src/models/models.dart
+++ packages/project_repository/lib/src/models/models.dart
@@ -1,5 +1,6 @@
 export 'pagination/pagination.dart';
 export 'project/project.dart';
+export 'project/fork/project_fork.dart';
 export 'tag/tag.dart';
 export 'comment/comment.dart';
 export 'filters.dart';
