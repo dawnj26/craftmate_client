53e8a91fadd81228313396076b41f561361c36b4
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-26 13:04:20 +0800

    add: prompt as param for generation

--- lib/project_management/create_project/generate_project/view/screens/view_suggestion_screen.dart
+++ lib/project_management/create_project/generate_project/view/screens/view_suggestion_screen.dart
@@ -1,17 +1,30 @@
+import 'package:craftmate_client/helpers/modal/modal.dart';
 import 'package:craftmate_client/helpers/transition/page_transition.dart';
+import 'package:craftmate_client/project_management/create_project/generate_project/bloc/view_suggestion/view_suggestion_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 import 'package:project_repository/project_repository.dart';
 
 class ViewSuggestionScreen extends StatelessWidget {
-  const ViewSuggestionScreen({super.key, required this.suggestion});
+  const ViewSuggestionScreen({
+    super.key,
+    required this.suggestion,
+    required this.prompt,
+  });
 
   final ProjectSuggestion suggestion;
+  final Prompt prompt;
 
   static Route<void> route({
     required ProjectSuggestion suggestion,
+    required Prompt prompt,
   }) {
     return PageTransition.effect.slideFromRightToLeft(
-      ViewSuggestionScreen(suggestion: suggestion),
+      ViewSuggestionScreen(
+        suggestion: suggestion,
+        prompt: prompt,
+      ),
     );
   }
 
