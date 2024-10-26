74d9d18a2b1b2ed133432ee2a170f704ee2a6ff5
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-26 13:04:52 +0800

    add: handlers for different states

--- lib/project_management/create_project/generate_project/view/screens/view_suggestion_screen.dart
+++ lib/project_management/create_project/generate_project/view/screens/view_suggestion_screen.dart
@@ -30,10 +30,184 @@ class ViewSuggestionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ViewSuggestionBloc(
+        projectRepo: context.read(),
+        prompt: prompt,
+      )..add(ViewSuggestionEvent.started(suggestion: suggestion)),
+      child: BlocConsumer<ViewSuggestionBloc, ViewSuggestionState>(
+        listener: (context, state) {
+          switch (state) {
+            case Error(message: final message):
+              Navigator.pop(context);
+              Modal.instance.showConfirmationModal(
+                context: context,
+                content: Text(message),
+                title: 'Error',
+                actions: [
+                  FilledButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Ok'),
+                  ),
+                ],
+              );
+            case Saving():
+              Modal.instance.showLoadingDialog(context);
+            case Saved():
+              Navigator.pop(context);
+              Modal.instance.showConfirmationModal(
+                context: context,
+                content: const Text('Project Saved successfully'),
+                title: 'Success',
+                actions: [
+                  FilledButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Ok'),
+                  ),
+                ],
+              );
+          }
+        },
+        builder: (context, state) {
+          switch (state) {
+            case Initial():
+            case Loading():
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(suggestion.title),
+                ),
+                body: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            default:
+              return _Suggestions(suggestion: state.pSuggestion);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class _Suggestions extends StatelessWidget {
+  const _Suggestions({
+    required this.suggestion,
+  });
+
+  final ProjectSuggestion suggestion;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(suggestion.title),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(12),
+              children: [
+                _Section(
+                  titleText: 'Description',
+                  contentText: suggestion.description,
+                ),
+                const Gap(24.0),
+                _ListSection(
+                  contentList: suggestion.materials.map((e) => e.name).toList(),
+                  titleText: 'Materials',
+                ),
+                const Gap(24.0),
+                _ListSection(
+                  titleText: 'Steps',
+                  contentList: suggestion.steps ?? [],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: FilledButton(
+              onPressed: () {
+                context.read<ViewSuggestionBloc>().add(
+                      const ViewSuggestionEvent.suggestionSaved(),
+                    );
+              },
+              child: const Text('Save suggestion'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ListSection extends StatelessWidget {
+  const _ListSection({required this.contentList, required this.titleText});
+
+  final String titleText;
+  final List<String> contentList;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          titleText,
+          style: theme.textTheme.titleMedium,
+        ),
+        const Divider(
+          height: 24,
+        ),
+        ...contentList.map(
+          (e) => Card(
+            color: theme.colorScheme.secondaryContainer,
+            child: ListTile(
+              title: Text(
+                e,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Section extends StatelessWidget {
+  const _Section({
+    required this.titleText,
+    required this.contentText,
+  });
+
+  final String titleText;
+  final String contentText;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          titleText,
+          style: theme.textTheme.titleMedium,
+        ),
+        const Divider(
+          height: 24,
+        ),
+        Text(
+          contentText,
+          style: theme.textTheme.bodyLarge,
+        ),
+      ],
     );
   }
 }
