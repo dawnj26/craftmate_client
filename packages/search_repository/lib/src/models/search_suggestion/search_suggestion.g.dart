21dd157af27ea952be391d1dd67b6810bddeb308
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-02 16:54:05 +0800

    add: search suggestion model

---
+++ packages/search_repository/lib/src/models/search_suggestion/search_suggestion.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'search_suggestion.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$SearchSuggestionImpl _$$SearchSuggestionImplFromJson(
+        Map<String, dynamic> json) =>
+    _$SearchSuggestionImpl(
+      type: json['type'] as String? ?? '',
+      value: json['value'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$$SearchSuggestionImplToJson(
+        _$SearchSuggestionImpl instance) =>
+    <String, dynamic>{
+      'type': instance.type,
+      'value': instance.value,
+    };
