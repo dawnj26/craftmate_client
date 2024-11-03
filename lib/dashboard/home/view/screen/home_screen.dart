dd5b1aa03503fc94a6dd3111fe3729e09c2368c4
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-03 20:24:25 +0800

    add: route to profile page

--- lib/dashboard/home/view/screen/home_screen.dart
+++ lib/dashboard/home/view/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:craftmate_client/auth/bloc/auth_bloc.dart';
 import 'package:craftmate_client/dashboard/home/view/tabs/following_tab_page.dart';
 import 'package:craftmate_client/dashboard/home/view/tabs/for_you_tab.dart';
 import 'package:craftmate_client/dashboard/home/view/tabs/trending_tab_page.dart';
+import 'package:craftmate_client/dashboard/profile/view/profile_page.dart';
 import 'package:craftmate_client/gen/assets.gen.dart';
 import 'package:craftmate_client/helpers/modal/modal.dart';
 import 'package:craftmate_client/project_management/view/create_project_page.dart';
@@ -86,7 +87,9 @@ class _HomeScreenState extends State<HomeScreen>
                 icon: const Icon(Icons.notifications_outlined),
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(context, ProfilePage.route());
+                },
                 icon: const Icon(Icons.person_2_outlined),
               ),
             ],
