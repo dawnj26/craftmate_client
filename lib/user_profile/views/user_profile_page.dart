948c39c0b17980bb92897545d0b5c9e9096c13d5
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-09 15:43:13 +0800

    add userId prop and provide view profile bloc

--- lib/user_profile/views/user_profile_page.dart
+++ lib/user_profile/views/user_profile_page.dart
@@ -1,17 +1,30 @@
+import 'package:craftmate_client/user_profile/bloc/view_profile/view_profile_bloc.dart';
 import 'package:craftmate_client/user_profile/views/screens/user_profile_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UserProfilePage extends StatelessWidget {
-  const UserProfilePage({super.key});
+  const UserProfilePage({super.key, required this.id});
 
-  static Route<void> route() {
+  final int id;
+
+  static Route<void> route(int userId) {
     return MaterialPageRoute(
-      builder: (context) => const UserProfilePage(),
+      builder: (context) => UserProfilePage(
+        id: userId,
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return const UserProfileScreen();
+    return BlocProvider(
+      create: (context) => ViewProfileBloc(
+        userRepository: context.read(),
+      )..add(
+          ViewProfileEvent.started(id),
+        ),
+      child: const UserProfileScreen(),
+    );
   }
 }
