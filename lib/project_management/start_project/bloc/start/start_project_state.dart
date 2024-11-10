d571aa99b1fc8e74c6eb7e0065ab5f7f27c6df20
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-10 10:26:47 +0800

    add: start project bloc for managing project progress

---
+++ lib/project_management/start_project/bloc/start/start_project_state.dart
@@ -0,0 +1,35 @@
+part of 'start_project_bloc.dart';
+
+@freezed
+class StartProjectState with _$StartProjectState {
+  const factory StartProjectState.initial({
+    required Project project,
+    @Default([]) List<bool> completedSteps,
+  }) = Initial;
+
+  const factory StartProjectState.saving({
+    required Project project,
+    @Default([]) List<bool> completedSteps,
+  }) = Saving;
+
+  const factory StartProjectState.finished({
+    required Project project,
+    @Default([]) List<bool> completedSteps,
+  }) = Finished;
+
+  const factory StartProjectState.loading({
+    required Project project,
+    @Default([]) List<bool> completedSteps,
+  }) = Loading;
+
+  const factory StartProjectState.loaded({
+    required Project project,
+    @Default([]) List<bool> completedSteps,
+  }) = Loaded;
+
+  const factory StartProjectState.failed({
+    required String errMessage,
+    required Project project,
+    @Default([]) List<bool> completedSteps,
+  }) = Failed;
+}
