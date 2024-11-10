ef3926f03004ff677e91130369e0a3e7008ff763
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-10 14:05:57 +0800

    add: implement ChatBloc with events and states, and create ChatPage and ChatScreen views

---
+++ lib/dashboard/chat/views/chat_page.dart
@@ -0,0 +1,11 @@
+import 'package:craftmate_client/dashboard/chat/views/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ChatScreen();
+  }
+}
