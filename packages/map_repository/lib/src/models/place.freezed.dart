8ab78fcca2bf6048da4d4977d0d6c653d7f2bb24
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-16 17:39:39 +0800

    add: implement Place model with JSON serialization

---
+++ packages/map_repository/lib/src/models/place.freezed.dart
@@ -0,0 +1,232 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'place.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Place _$PlaceFromJson(Map<String, dynamic> json) {
+  return _Place.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Place {
+  @JsonKey(name: 'place_id')
+  String get placeId => throw _privateConstructorUsedError;
+  @JsonKey(name: 'formatted')
+  String get name => throw _privateConstructorUsedError;
+  double get lat => throw _privateConstructorUsedError;
+  double get lon => throw _privateConstructorUsedError;
+
+  /// Serializes this Place to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of Place
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $PlaceCopyWith<Place> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PlaceCopyWith<$Res> {
+  factory $PlaceCopyWith(Place value, $Res Function(Place) then) =
+      _$PlaceCopyWithImpl<$Res, Place>;
+  @useResult
+  $Res call(
+      {@JsonKey(name: 'place_id') String placeId,
+      @JsonKey(name: 'formatted') String name,
+      double lat,
+      double lon});
+}
+
+/// @nodoc
+class _$PlaceCopyWithImpl<$Res, $Val extends Place>
+    implements $PlaceCopyWith<$Res> {
+  _$PlaceCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Place
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? placeId = null,
+    Object? name = null,
+    Object? lat = null,
+    Object? lon = null,
+  }) {
+    return _then(_value.copyWith(
+      placeId: null == placeId
+          ? _value.placeId
+          : placeId // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      lat: null == lat
+          ? _value.lat
+          : lat // ignore: cast_nullable_to_non_nullable
+              as double,
+      lon: null == lon
+          ? _value.lon
+          : lon // ignore: cast_nullable_to_non_nullable
+              as double,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$PlaceImplCopyWith<$Res> implements $PlaceCopyWith<$Res> {
+  factory _$$PlaceImplCopyWith(
+          _$PlaceImpl value, $Res Function(_$PlaceImpl) then) =
+      __$$PlaceImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {@JsonKey(name: 'place_id') String placeId,
+      @JsonKey(name: 'formatted') String name,
+      double lat,
+      double lon});
+}
+
+/// @nodoc
+class __$$PlaceImplCopyWithImpl<$Res>
+    extends _$PlaceCopyWithImpl<$Res, _$PlaceImpl>
+    implements _$$PlaceImplCopyWith<$Res> {
+  __$$PlaceImplCopyWithImpl(
+      _$PlaceImpl _value, $Res Function(_$PlaceImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Place
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? placeId = null,
+    Object? name = null,
+    Object? lat = null,
+    Object? lon = null,
+  }) {
+    return _then(_$PlaceImpl(
+      placeId: null == placeId
+          ? _value.placeId
+          : placeId // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      lat: null == lat
+          ? _value.lat
+          : lat // ignore: cast_nullable_to_non_nullable
+              as double,
+      lon: null == lon
+          ? _value.lon
+          : lon // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$PlaceImpl implements _Place {
+  const _$PlaceImpl(
+      {@JsonKey(name: 'place_id') this.placeId = '',
+      @JsonKey(name: 'formatted') this.name = '',
+      this.lat = 0.0,
+      this.lon = 0.0});
+
+  factory _$PlaceImpl.fromJson(Map<String, dynamic> json) =>
+      _$$PlaceImplFromJson(json);
+
+  @override
+  @JsonKey(name: 'place_id')
+  final String placeId;
+  @override
+  @JsonKey(name: 'formatted')
+  final String name;
+  @override
+  @JsonKey()
+  final double lat;
+  @override
+  @JsonKey()
+  final double lon;
+
+  @override
+  String toString() {
+    return 'Place(placeId: $placeId, name: $name, lat: $lat, lon: $lon)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$PlaceImpl &&
+            (identical(other.placeId, placeId) || other.placeId == placeId) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.lat, lat) || other.lat == lat) &&
+            (identical(other.lon, lon) || other.lon == lon));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(runtimeType, placeId, name, lat, lon);
+
+  /// Create a copy of Place
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$PlaceImplCopyWith<_$PlaceImpl> get copyWith =>
+      __$$PlaceImplCopyWithImpl<_$PlaceImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$PlaceImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Place implements Place {
+  const factory _Place(
+      {@JsonKey(name: 'place_id') final String placeId,
+      @JsonKey(name: 'formatted') final String name,
+      final double lat,
+      final double lon}) = _$PlaceImpl;
+
+  factory _Place.fromJson(Map<String, dynamic> json) = _$PlaceImpl.fromJson;
+
+  @override
+  @JsonKey(name: 'place_id')
+  String get placeId;
+  @override
+  @JsonKey(name: 'formatted')
+  String get name;
+  @override
+  double get lat;
+  @override
+  double get lon;
+
+  /// Create a copy of Place
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$PlaceImplCopyWith<_$PlaceImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
