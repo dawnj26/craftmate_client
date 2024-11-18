acb6246506e61326ff85c74b3c802a4c0a521eb2
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-18 19:54:57 +0800

    add: implement ShopBloc with event handling and repository integration

--- lib/dashboard/shop/bloc/shop/shop_bloc.dart
+++ lib/dashboard/shop/bloc/shop/shop_bloc.dart
@@ -1,14 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:shop_repository/shop_repository.dart';
 
 part 'shop_event.dart';
 part 'shop_state.dart';
 part 'shop_bloc.freezed.dart';
 
 class ShopBloc extends Bloc<ShopEvent, ShopState> {
-  ShopBloc() : super(const Initial()) {
-    on<ShopEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  ShopBloc({
+    required ShopRepository shopRepository,
+  })  : _shopRepository = shopRepository,
+        super(const Initial()) {
+    on<_Started>(_onStarted);
+  }
+
+  final ShopRepository _shopRepository;
+
+  Future<void> _onStarted(_Started event, Emitter<ShopState> emit) async {
+    emit(const Loading());
+    try {
+      final products = await _shopRepository.fetchListings();
+      emit(Loaded(products: products));
+    } on ShopException catch (e) {
+      emit(Error(message: e.message));
+    }
   }
 }
