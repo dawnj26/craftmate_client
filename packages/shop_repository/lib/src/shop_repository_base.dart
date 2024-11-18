f29f65c87c4246ea66f6be030e0cc64775629f02
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-18 19:05:04 +0800

    add: extend IShopRepository with publishListing method

--- packages/shop_repository/lib/src/shop_repository_base.dart
+++ packages/shop_repository/lib/src/shop_repository_base.dart
@@ -1,6 +1,8 @@
 import 'package:config_repository/config_repository.dart';
 
-abstract class IShopRepository {}
+abstract class IShopRepository {
+  Future<void> publishListing(Product product);
+}
 
 class ShopRepository implements IShopRepository {
   final ConfigRepository _config;
