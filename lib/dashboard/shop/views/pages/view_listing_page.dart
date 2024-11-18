50b4d13229f16567711f9035a03b8ea9377f8594
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-18 20:12:55 +0800

    add: pass product data to ViewListingPage and implement route method

--- lib/dashboard/shop/views/pages/view_listing_page.dart
+++ lib/dashboard/shop/views/pages/view_listing_page.dart
@@ -1,8 +1,17 @@
 import 'package:craftmate_client/dashboard/shop/views/screens/view_listing_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_repository/shop_repository.dart';
 
 class ViewListingPage extends StatelessWidget {
-  const ViewListingPage({super.key});
+  const ViewListingPage({super.key, required this.product});
+
+  final Product product;
+
+  static Route<void> route(Product product) {
+    return MaterialPageRoute<void>(
+      builder: (_) => ViewListingPage(product: product),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
