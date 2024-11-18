c3d88c6faf6923c8fa6a5565ec7046044fadc5a8
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-18 19:05:46 +0800

    add: implement publishListing method in ShopRepository

--- packages/shop_repository/lib/src/shop_repository_base.dart
+++ packages/shop_repository/lib/src/shop_repository_base.dart
@@ -8,4 +8,21 @@ class ShopRepository implements IShopRepository {
   final ConfigRepository _config;
 
   const ShopRepository(this._config);
+
+  @override
+  Future<void> publishListing(Product product) async {
+    try {
+      final List<String> imageUrls = await _uploadImages(product.imageUrls);
+      final newProduct = product.copyWith(
+        imageUrls: imageUrls,
+      );
+
+      await _config.db.collection('marketplace').add(newProduct.toJson());
+    } catch (e) {
+      _config.logger
+          .error('Failed to publish listing: $e', e, StackTrace.current);
+      throw ShopException('Failed to publish listing: $e');
+    }
+  }
+
 }
