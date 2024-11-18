cf2c5515c72e21c44e47ee43bf81b7f11aef6e87
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-18 19:09:01 +0800

    add: pass onAddressSelected callback to AddAddressScreen for address selection

--- lib/dashboard/shop/views/pages/add_address_page.dart
+++ lib/dashboard/shop/views/pages/add_address_page.dart
@@ -1,17 +1,26 @@
 import 'package:craftmate_client/dashboard/shop/views/screens/add_address_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:map_repository/map_repository.dart';
 
 class AddAddressPage extends StatelessWidget {
-  const AddAddressPage({super.key});
+  const AddAddressPage({super.key, this.onAddressSelected});
 
-  static Route<void> route() {
+  final void Function(Place place)? onAddressSelected;
+
+  static Route<void> route({
+    void Function(Place place)? onAddressSelected,
+  }) {
     return MaterialPageRoute<void>(
-      builder: (_) => const AddAddressPage(),
+      builder: (_) => AddAddressPage(
+        onAddressSelected: onAddressSelected,
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return const AddAddressScreen();
+    return AddAddressScreen(
+      onAddressSelected: onAddressSelected,
+    );
   }
 }
