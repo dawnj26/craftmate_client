90f404cbc5ad907a5b02786e498b18fe1db9a425
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-18 20:48:45 +0800

    add: fetch product

--- lib/dashboard/shop/bloc/view_listing/view_listing_bloc.dart
+++ lib/dashboard/shop/bloc/view_listing/view_listing_bloc.dart
@@ -1,14 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:shop_repository/shop_repository.dart';
 
 part 'view_listing_event.dart';
 part 'view_listing_state.dart';
 part 'view_listing_bloc.freezed.dart';
 
 class ViewListingBloc extends Bloc<ViewListingEvent, ViewListingState> {
-  ViewListingBloc() : super(const Initial()) {
-    on<ViewListingEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  ViewListingBloc({
+    required ShopRepository shopRepository,
+  })  : _shopRepository = shopRepository,
+        super(const Initial()) {
+    on<_Started>(_onStarted);
+  }
+
+  final ShopRepository _shopRepository;
+
+  Future<void> _onStarted(
+      _Started event, Emitter<ViewListingState> emit) async {
+    emit(const Loading());
+    try {
+      final product = await _shopRepository.fetchListing(event.id);
+      emit(Loaded(product: product));
+    } on ShopException catch (e) {
+      emit(Error(message: e.message));
+    }
   }
 }
