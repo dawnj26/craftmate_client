715172d71fb2440a08ff2156f6e1521669f3677c
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-18 20:49:04 +0800

    refactor: update ViewListingPage to use QueryProduct and integrate Bloc for event handling

--- lib/dashboard/shop/views/pages/view_listing_page.dart
+++ lib/dashboard/shop/views/pages/view_listing_page.dart
@@ -1,20 +1,27 @@
+import 'package:craftmate_client/dashboard/shop/bloc/view_listing/view_listing_bloc.dart';
 import 'package:craftmate_client/dashboard/shop/views/screens/view_listing_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_repository/shop_repository.dart';
 
 class ViewListingPage extends StatelessWidget {
-  const ViewListingPage({super.key, required this.product});
+  const ViewListingPage({super.key, required this.query});
 
-  final Product product;
+  final QueryProduct query;
 
-  static Route<void> route(Product product) {
+  static Route<void> route(QueryProduct query) {
     return MaterialPageRoute<void>(
-      builder: (_) => ViewListingPage(product: product),
+      builder: (_) => ViewListingPage(query: query),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return const ViewListingScreen();
+    return BlocProvider(
+      create: (context) => ViewListingBloc(
+        shopRepository: context.read(),
+      )..add(ViewListingEvent.started(query.id)),
+      child: const ViewListingScreen(),
+    );
   }
 }
