d8fd602d2bee730af624cab3eb1177a6f91184bc
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-20 00:42:28 +0800

    refactor: integrate UserRepository into ViewListingBloc for seller data retrieval

--- lib/dashboard/shop/bloc/view_listing/view_listing_bloc.dart
+++ lib/dashboard/shop/bloc/view_listing/view_listing_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:shop_repository/shop_repository.dart';
+import 'package:user_repository/user_repository.dart';
 
 part 'view_listing_event.dart';
 part 'view_listing_state.dart';
@@ -9,19 +10,26 @@ part 'view_listing_bloc.freezed.dart';
 class ViewListingBloc extends Bloc<ViewListingEvent, ViewListingState> {
   ViewListingBloc({
     required ShopRepository shopRepository,
-  })  : _shopRepository = shopRepository,
+    required UserRepository userRepository,
+  })  : _userRepository = userRepository,
+        _shopRepository = shopRepository,
         super(const Initial()) {
     on<_Started>(_onStarted);
   }
 
   final ShopRepository _shopRepository;
+  final UserRepository _userRepository;
 
   Future<void> _onStarted(
-      _Started event, Emitter<ViewListingState> emit) async {
+    _Started event,
+    Emitter<ViewListingState> emit,
+  ) async {
     emit(const Loading());
     try {
       final product = await _shopRepository.fetchListing(event.id);
-      emit(Loaded(product: product));
+      final seller =
+          await _userRepository.getUserById(product.product.sellerId);
+      emit(Loaded(query: product, seller: seller));
     } on ShopException catch (e) {
       emit(Error(message: e.message));
     }
