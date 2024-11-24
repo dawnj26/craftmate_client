7e4feef8cccb3e45a16e000d19a4ea8ebca89e14
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-25 00:18:24 +0800

    feat: implement loading and error handling in ShopPage with modal and snackbar alerts

--- lib/dashboard/shop/views/pages/shop_page.dart
+++ lib/dashboard/shop/views/pages/shop_page.dart
@@ -1,5 +1,7 @@
 import 'package:craftmate_client/dashboard/shop/bloc/shop/shop_bloc.dart';
 import 'package:craftmate_client/dashboard/shop/views/screens/shop_screen.dart';
+import 'package:craftmate_client/helpers/alert/alert.dart';
+import 'package:craftmate_client/helpers/modal/modal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,8 +13,22 @@ class ShopPage extends StatelessWidget {
     return BlocProvider(
       create: (context) => ShopBloc(
         shopRepository: context.read(),
+        mapRepository: context.read(),
       )..add(const ShopEvent.started()),
-      child: const ShopScreen(),
+      child: BlocListener<ShopBloc, ShopState>(
+        listener: (context, state) {
+          switch (state) {
+            case Loading():
+              Modal.instance.showLoadingDialog(context);
+            case Loaded():
+              Navigator.of(context).pop();
+            case Error(:final message):
+              Navigator.of(context).pop();
+              Alert.instance.showSnackbar(context, message);
+          }
+        },
+        child: const ShopScreen(),
+      ),
     );
   }
 }
