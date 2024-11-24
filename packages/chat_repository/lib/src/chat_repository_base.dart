65a8a28d3a05e700feaab44a2819fd6ef64d91e8
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-25 00:21:24 +0800

    feat: enhance ChatRepository with dynamic chat collection handling and add hasMessages method

--- packages/chat_repository/lib/src/chat_repository_base.dart
+++ packages/chat_repository/lib/src/chat_repository_base.dart
@@ -9,6 +9,7 @@ import 'package:user_repository/user_repository.dart';
 abstract class IChatRepository {
   Future<void> sendMessage(Message message);
   Stream<List<Message>> messages(int senderId, int receiverId);
+  Future<bool> hasMessages(int senderId, int receiverId);
   Stream<List<Chat>> chats(int userId);
 }
 
@@ -16,11 +17,13 @@ class ChatRepository implements IChatRepository {
   final ConfigRepository _config;
   final UserRepository _userRepository;
 
-  const ChatRepository({
+  ChatRepository({
     required ConfigRepository config,
     required UserRepository userRepository,
   })  : _config = config,
         _userRepository = userRepository;
+
+  String _baseCollection = 'chats';
 
   String _getConversationId(int userId1, int userId2) {
     final sortedIds = [userId1, userId2]..sort();
@@ -93,11 +96,17 @@ class ChatRepository implements IChatRepository {
   }
 
   @override
-  Future<void> sendMessage(Message message) async {
+  Future<void> sendMessage(Message message, {String? listingId}) async {
     final conversationId = _getConversationId(
       message.senderId,
       message.receiverId,
     );
+
+    if (listingId != null) {
+      _baseCollection = 'shop/$listingId/chats';
+    } else {
+      _baseCollection = 'chats';
+    }
 
     switch (message.type) {
       case MessageType.text:
@@ -111,16 +120,18 @@ class ChatRepository implements IChatRepository {
   }
 
   @override
-  Stream<List<Message>> messages(int senderId, int receiverId) {
+  Stream<List<Message>> messages(int senderId, int receiverId,
+      {String? listingId}) {
     final conversationId = _getConversationId(senderId, receiverId);
 
+    final collection = listingId != null ? 'shop/$listingId/chats' : 'chats';
     return _config.db
-        .collection('chats/$conversationId/messages')
+        .collection('$collection/$conversationId/messages')
         .orderBy('createdAt', descending: true)
         .snapshots()
         .asyncMap((snapshot) async {
       await _config.db
-          .collection('chats')
+          .collection(collection)
           .doc(conversationId)
           .update({'$senderId': DateTime.now().toIso8601String()});
 
@@ -132,9 +143,11 @@ class ChatRepository implements IChatRepository {
   }
 
   @override
-  Stream<List<Chat>> chats(int userId) {
+  Stream<List<Chat>> chats(int userId, {String? listingId}) {
+    final collection = listingId != null ? 'shop/$listingId/chats' : 'chats';
+
     return _config.db
-        .collection('chats')
+        .collection(collection)
         .where('userIds', arrayContains: userId)
         .snapshots()
         .asyncMap((snapshot) async {
@@ -163,7 +176,7 @@ class ChatRepository implements IChatRepository {
   }
 
   Future<void> _setLatestMessage(Message message, String conversationId) async {
-    await _config.db.collection('chats').doc(conversationId).set({
+    await _config.db.collection(_baseCollection).doc(conversationId).set({
       'userIds': [message.senderId, message.receiverId]..sort(),
       '${message.senderId}': DateTime.now().toIso8601String(),
       '${message.receiverId}': null,
@@ -173,7 +186,21 @@ class ChatRepository implements IChatRepository {
 
   Future<void> _addMessage(Message message, String conversationId) async {
     await _config.db
-        .collection('chats/$conversationId/messages')
+        .collection('$_baseCollection/$conversationId/messages')
         .add(message.toJson());
+  }
+
+  @override
+  Future<bool> hasMessages(int senderId, int receiverId) async {
+    try {
+      final docSnapshot = await _config.db
+          .collection('chats')
+          .doc(_getConversationId(senderId, receiverId))
+          .get();
+      return docSnapshot.exists;
+    } catch (e) {
+      _config.logger.error('Error checking document existence: $e');
+      return false;
+    }
   }
 }
