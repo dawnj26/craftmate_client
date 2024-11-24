0b6edda9316e40dd013943e6f627c9e415711ea6
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-25 00:17:09 +0800

    feat: implement ShopActivityBloc with initial event and state definitions

---
+++ lib/dashboard/shop/bloc/activity/shop_activity_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'shop_activity_event.dart';
+part 'shop_activity_state.dart';
+part 'shop_activity_bloc.freezed.dart';
+
+class ShopActivityBloc extends Bloc<ShopActivityEvent, ShopActivityState> {
+  ShopActivityBloc() : super(_Initial()) {
+    on<ShopActivityEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
