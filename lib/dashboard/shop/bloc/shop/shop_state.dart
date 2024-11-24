6c6ab76b2948d1d60a7b3487e0f24c820c9300f3
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-25 00:17:20 +0800

    feat: extend ShopState with nearbyProducts and currentLocation for enhanced state management

--- lib/dashboard/shop/bloc/shop/shop_state.dart
+++ lib/dashboard/shop/bloc/shop/shop_state.dart
@@ -4,18 +4,36 @@ part of 'shop_bloc.dart';
 class ShopState with _$ShopState {
   const factory ShopState.initial({
     @Default(<QueryProduct>[]) List<QueryProduct> products,
+    @Default(<QueryProduct>[]) List<QueryProduct> nearbyProducts,
+    @Default(Place()) Place currentLocation,
   }) = Initial;
+  const factory ShopState.initializing({
+    @Default(<QueryProduct>[]) List<QueryProduct> products,
+    @Default(<QueryProduct>[]) List<QueryProduct> nearbyProducts,
+    @Default(Place()) Place currentLocation,
+  }) = Initializing;
+  const factory ShopState.initialized({
+    @Default(<QueryProduct>[]) List<QueryProduct> products,
+    @Default(<QueryProduct>[]) List<QueryProduct> nearbyProducts,
+    @Default(Place()) Place currentLocation,
+  }) = Initialized;
 
   const factory ShopState.loading({
     @Default(<QueryProduct>[]) List<QueryProduct> products,
+    @Default(<QueryProduct>[]) List<QueryProduct> nearbyProducts,
+    @Default(Place()) Place currentLocation,
   }) = Loading;
 
   const factory ShopState.loaded({
     required List<QueryProduct> products,
+    required List<QueryProduct> nearbyProducts,
+    @Default(Place()) Place currentLocation,
   }) = Loaded;
 
   const factory ShopState.error({
     required String message,
     @Default(<QueryProduct>[]) List<QueryProduct> products,
+    @Default(<QueryProduct>[]) List<QueryProduct> nearbyProducts,
+    @Default(Place()) Place currentLocation,
   }) = Error;
 }
