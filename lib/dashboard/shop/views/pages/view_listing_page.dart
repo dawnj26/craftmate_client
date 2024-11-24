3e4bf560936a15193768baff8852cf5bb402856b
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-25 00:18:52 +0800

    feat: add chat navigation and error handling in ViewListingPage with snackbar and loading dialog

--- lib/dashboard/shop/views/pages/view_listing_page.dart
+++ lib/dashboard/shop/views/pages/view_listing_page.dart
@@ -1,5 +1,8 @@
+import 'package:craftmate_client/dashboard/chats/views/screens/chat_screen.dart';
 import 'package:craftmate_client/dashboard/shop/bloc/view_listing/view_listing_bloc.dart';
 import 'package:craftmate_client/dashboard/shop/views/screens/view_listing_screen.dart';
+import 'package:craftmate_client/helpers/alert/alert.dart';
+import 'package:craftmate_client/helpers/modal/modal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_repository/shop_repository.dart';
@@ -21,8 +24,23 @@ class ViewListingPage extends StatelessWidget {
       create: (context) => ViewListingBloc(
         shopRepository: context.read(),
         userRepository: context.read(),
+        chatRepository: context.read(),
       )..add(ViewListingEvent.started(query.id)),
-      child: const ViewListingScreen(),
+      child: BlocListener<ViewListingBloc, ViewListingState>(
+        listener: (context, state) {
+          switch (state) {
+            case Error(:final errMessage):
+              Alert.instance.showSnackbar(context, errMessage);
+            case Sending():
+              Modal.instance.showLoadingDialog(context);
+            case Sent(:final seller, :final query):
+              Navigator.of(context).pushReplacement(
+                ChatScreen.route(seller, listingId: query.id),
+              );
+          }
+        },
+        child: const ViewListingScreen(),
+      ),
     );
   }
 }
