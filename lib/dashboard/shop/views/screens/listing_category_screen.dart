7af2a37a647c0ce4bc4e290f0b4ee668be17413e
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-25 12:36:58 +0800

    feat: add ListingCategoryPage and ListingCategoryScreen for category-based product listing

---
+++ lib/dashboard/shop/views/screens/listing_category_screen.dart
@@ -0,0 +1,52 @@
+import 'package:craftmate_client/dashboard/shop/bloc/listing_category/listing_category_bloc.dart';
+import 'package:craftmate_client/dashboard/shop/views/pages/view_listing_page.dart';
+import 'package:craftmate_client/dashboard/shop/views/screens/shop_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ListingCategoryScreen extends StatelessWidget {
+  const ListingCategoryScreen({super.key, required this.category});
+
+  final String category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category),
+      ),
+      body: BlocBuilder<ListingCategoryBloc, ListingCategoryState>(
+        builder: (context, state) {
+          switch (state) {
+            case Initial() || Loading():
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+          }
+
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 4,
+              mainAxisSpacing: 8,
+              childAspectRatio: 0.9,
+            ),
+            itemBuilder: (context, index) {
+              final product = state.query[index].product;
+              return ListingTile(
+                product: product,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    ViewListingPage.route(state.query[index]),
+                  );
+                },
+              );
+            },
+            itemCount: state.query.length,
+          );
+        },
+      ),
+    );
+  }
+}
