f5892df25ed1be6b27ba6707b234e8cab90d27e1
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-25 12:36:25 +0800

    feat: implement ListingCategoryBloc with events and states for category-based listing management

---
+++ lib/dashboard/shop/bloc/listing_category/listing_category_bloc.freezed.dart
@@ -0,0 +1,573 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'listing_category_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+
+final _privateConstructorUsedError = UnsupportedError('It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$ListingCategoryEvent {
+
+ String get category => throw _privateConstructorUsedError;
+
+
+
+
+
+
+
+/// Create a copy of ListingCategoryEvent
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+$ListingCategoryEventCopyWith<ListingCategoryEvent> get copyWith => throw _privateConstructorUsedError;
+
+}
+
+/// @nodoc
+abstract class $ListingCategoryEventCopyWith<$Res>  {
+  factory $ListingCategoryEventCopyWith(ListingCategoryEvent value, $Res Function(ListingCategoryEvent) then) = _$ListingCategoryEventCopyWithImpl<$Res, ListingCategoryEvent>;
+@useResult
+$Res call({
+ String category
+});
+
+
+
+}
+
+/// @nodoc
+class _$ListingCategoryEventCopyWithImpl<$Res,$Val extends ListingCategoryEvent> implements $ListingCategoryEventCopyWith<$Res> {
+  _$ListingCategoryEventCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+/// Create a copy of ListingCategoryEvent
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? category = null,}) {
+  return _then(_value.copyWith(
+category: null == category ? _value.category : category // ignore: cast_nullable_to_non_nullable
+as String,
+  )as $Val);
+}
+
+}
+
+
+/// @nodoc
+abstract class _$$StartedImplCopyWith<$Res> implements $ListingCategoryEventCopyWith<$Res> {
+  factory _$$StartedImplCopyWith(_$StartedImpl value, $Res Function(_$StartedImpl) then) = __$$StartedImplCopyWithImpl<$Res>;
+@override @useResult
+$Res call({
+ String category
+});
+
+
+
+}
+
+/// @nodoc
+class __$$StartedImplCopyWithImpl<$Res> extends _$ListingCategoryEventCopyWithImpl<$Res, _$StartedImpl> implements _$$StartedImplCopyWith<$Res> {
+  __$$StartedImplCopyWithImpl(_$StartedImpl _value, $Res Function(_$StartedImpl) _then)
+      : super(_value, _then);
+
+
+/// Create a copy of ListingCategoryEvent
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? category = null,}) {
+  return _then(_$StartedImpl(
+null == category ? _value.category : category // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
+}
+
+
+}
+
+/// @nodoc
+
+
+class _$StartedImpl  implements _Started {
+  const _$StartedImpl(this.category);
+
+  
+
+@override final  String category;
+
+@override
+String toString() {
+  return 'ListingCategoryEvent.started(category: $category)';
+}
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _$StartedImpl&&(identical(other.category, category) || other.category == category));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,category);
+
+/// Create a copy of ListingCategoryEvent
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+@pragma('vm:prefer-inline')
+_$$StartedImplCopyWith<_$StartedImpl> get copyWith => __$$StartedImplCopyWithImpl<_$StartedImpl>(this, _$identity);
+
+
+
+
+
+
+
+
+}
+
+
+abstract class _Started implements ListingCategoryEvent {
+  const factory _Started(final  String category) = _$StartedImpl;
+  
+
+  
+
+@override String get category;
+/// Create a copy of ListingCategoryEvent
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+_$$StartedImplCopyWith<_$StartedImpl> get copyWith => throw _privateConstructorUsedError;
+
+}
+
+/// @nodoc
+mixin _$ListingCategoryState {
+
+ List<QueryProduct> get query => throw _privateConstructorUsedError;
+
+
+
+
+
+
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+$ListingCategoryStateCopyWith<ListingCategoryState> get copyWith => throw _privateConstructorUsedError;
+
+}
+
+/// @nodoc
+abstract class $ListingCategoryStateCopyWith<$Res>  {
+  factory $ListingCategoryStateCopyWith(ListingCategoryState value, $Res Function(ListingCategoryState) then) = _$ListingCategoryStateCopyWithImpl<$Res, ListingCategoryState>;
+@useResult
+$Res call({
+ List<QueryProduct> query
+});
+
+
+
+}
+
+/// @nodoc
+class _$ListingCategoryStateCopyWithImpl<$Res,$Val extends ListingCategoryState> implements $ListingCategoryStateCopyWith<$Res> {
+  _$ListingCategoryStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? query = null,}) {
+  return _then(_value.copyWith(
+query: null == query ? _value.query : query // ignore: cast_nullable_to_non_nullable
+as List<QueryProduct>,
+  )as $Val);
+}
+
+}
+
+
+/// @nodoc
+abstract class _$$InitialImplCopyWith<$Res> implements $ListingCategoryStateCopyWith<$Res> {
+  factory _$$InitialImplCopyWith(_$InitialImpl value, $Res Function(_$InitialImpl) then) = __$$InitialImplCopyWithImpl<$Res>;
+@override @useResult
+$Res call({
+ List<QueryProduct> query
+});
+
+
+
+}
+
+/// @nodoc
+class __$$InitialImplCopyWithImpl<$Res> extends _$ListingCategoryStateCopyWithImpl<$Res, _$InitialImpl> implements _$$InitialImplCopyWith<$Res> {
+  __$$InitialImplCopyWithImpl(_$InitialImpl _value, $Res Function(_$InitialImpl) _then)
+      : super(_value, _then);
+
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? query = null,}) {
+  return _then(_$InitialImpl(
+query: null == query ? _value._query : query // ignore: cast_nullable_to_non_nullable
+as List<QueryProduct>,
+  ));
+}
+
+
+}
+
+/// @nodoc
+
+
+class _$InitialImpl  implements Initial {
+  const _$InitialImpl({final  List<QueryProduct> query = const []}): _query = query;
+
+  
+
+ final  List<QueryProduct> _query;
+@override@JsonKey() List<QueryProduct> get query {
+  if (_query is EqualUnmodifiableListView) return _query;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_query);
+}
+
+
+@override
+String toString() {
+  return 'ListingCategoryState.initial(query: $query)';
+}
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _$InitialImpl&&const DeepCollectionEquality().equals(other._query, _query));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(_query));
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+@pragma('vm:prefer-inline')
+_$$InitialImplCopyWith<_$InitialImpl> get copyWith => __$$InitialImplCopyWithImpl<_$InitialImpl>(this, _$identity);
+
+
+
+
+
+
+
+
+}
+
+
+abstract class Initial implements ListingCategoryState {
+  const factory Initial({final  List<QueryProduct> query}) = _$InitialImpl;
+  
+
+  
+
+@override List<QueryProduct> get query;
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+_$$InitialImplCopyWith<_$InitialImpl> get copyWith => throw _privateConstructorUsedError;
+
+}
+
+/// @nodoc
+abstract class _$$LoadingImplCopyWith<$Res> implements $ListingCategoryStateCopyWith<$Res> {
+  factory _$$LoadingImplCopyWith(_$LoadingImpl value, $Res Function(_$LoadingImpl) then) = __$$LoadingImplCopyWithImpl<$Res>;
+@override @useResult
+$Res call({
+ List<QueryProduct> query
+});
+
+
+
+}
+
+/// @nodoc
+class __$$LoadingImplCopyWithImpl<$Res> extends _$ListingCategoryStateCopyWithImpl<$Res, _$LoadingImpl> implements _$$LoadingImplCopyWith<$Res> {
+  __$$LoadingImplCopyWithImpl(_$LoadingImpl _value, $Res Function(_$LoadingImpl) _then)
+      : super(_value, _then);
+
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? query = null,}) {
+  return _then(_$LoadingImpl(
+query: null == query ? _value._query : query // ignore: cast_nullable_to_non_nullable
+as List<QueryProduct>,
+  ));
+}
+
+
+}
+
+/// @nodoc
+
+
+class _$LoadingImpl  implements Loading {
+  const _$LoadingImpl({final  List<QueryProduct> query = const []}): _query = query;
+
+  
+
+ final  List<QueryProduct> _query;
+@override@JsonKey() List<QueryProduct> get query {
+  if (_query is EqualUnmodifiableListView) return _query;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_query);
+}
+
+
+@override
+String toString() {
+  return 'ListingCategoryState.loading(query: $query)';
+}
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _$LoadingImpl&&const DeepCollectionEquality().equals(other._query, _query));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(_query));
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+@pragma('vm:prefer-inline')
+_$$LoadingImplCopyWith<_$LoadingImpl> get copyWith => __$$LoadingImplCopyWithImpl<_$LoadingImpl>(this, _$identity);
+
+
+
+
+
+
+
+
+}
+
+
+abstract class Loading implements ListingCategoryState {
+  const factory Loading({final  List<QueryProduct> query}) = _$LoadingImpl;
+  
+
+  
+
+@override List<QueryProduct> get query;
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+_$$LoadingImplCopyWith<_$LoadingImpl> get copyWith => throw _privateConstructorUsedError;
+
+}
+
+/// @nodoc
+abstract class _$$LoadedImplCopyWith<$Res> implements $ListingCategoryStateCopyWith<$Res> {
+  factory _$$LoadedImplCopyWith(_$LoadedImpl value, $Res Function(_$LoadedImpl) then) = __$$LoadedImplCopyWithImpl<$Res>;
+@override @useResult
+$Res call({
+ List<QueryProduct> query
+});
+
+
+
+}
+
+/// @nodoc
+class __$$LoadedImplCopyWithImpl<$Res> extends _$ListingCategoryStateCopyWithImpl<$Res, _$LoadedImpl> implements _$$LoadedImplCopyWith<$Res> {
+  __$$LoadedImplCopyWithImpl(_$LoadedImpl _value, $Res Function(_$LoadedImpl) _then)
+      : super(_value, _then);
+
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? query = null,}) {
+  return _then(_$LoadedImpl(
+query: null == query ? _value._query : query // ignore: cast_nullable_to_non_nullable
+as List<QueryProduct>,
+  ));
+}
+
+
+}
+
+/// @nodoc
+
+
+class _$LoadedImpl  implements Loaded {
+  const _$LoadedImpl({final  List<QueryProduct> query = const []}): _query = query;
+
+  
+
+ final  List<QueryProduct> _query;
+@override@JsonKey() List<QueryProduct> get query {
+  if (_query is EqualUnmodifiableListView) return _query;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_query);
+}
+
+
+@override
+String toString() {
+  return 'ListingCategoryState.loaded(query: $query)';
+}
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _$LoadedImpl&&const DeepCollectionEquality().equals(other._query, _query));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(_query));
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+@pragma('vm:prefer-inline')
+_$$LoadedImplCopyWith<_$LoadedImpl> get copyWith => __$$LoadedImplCopyWithImpl<_$LoadedImpl>(this, _$identity);
+
+
+
+
+
+
+
+
+}
+
+
+abstract class Loaded implements ListingCategoryState {
+  const factory Loaded({final  List<QueryProduct> query}) = _$LoadedImpl;
+  
+
+  
+
+@override List<QueryProduct> get query;
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+_$$LoadedImplCopyWith<_$LoadedImpl> get copyWith => throw _privateConstructorUsedError;
+
+}
+
+/// @nodoc
+abstract class _$$ErrorImplCopyWith<$Res> implements $ListingCategoryStateCopyWith<$Res> {
+  factory _$$ErrorImplCopyWith(_$ErrorImpl value, $Res Function(_$ErrorImpl) then) = __$$ErrorImplCopyWithImpl<$Res>;
+@override @useResult
+$Res call({
+ String message, List<QueryProduct> query
+});
+
+
+
+}
+
+/// @nodoc
+class __$$ErrorImplCopyWithImpl<$Res> extends _$ListingCategoryStateCopyWithImpl<$Res, _$ErrorImpl> implements _$$ErrorImplCopyWith<$Res> {
+  __$$ErrorImplCopyWithImpl(_$ErrorImpl _value, $Res Function(_$ErrorImpl) _then)
+      : super(_value, _then);
+
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? message = null,Object? query = null,}) {
+  return _then(_$ErrorImpl(
+message: null == message ? _value.message : message // ignore: cast_nullable_to_non_nullable
+as String,query: null == query ? _value._query : query // ignore: cast_nullable_to_non_nullable
+as List<QueryProduct>,
+  ));
+}
+
+
+}
+
+/// @nodoc
+
+
+class _$ErrorImpl  implements Error {
+  const _$ErrorImpl({required this.message, final  List<QueryProduct> query = const []}): _query = query;
+
+  
+
+@override final  String message;
+ final  List<QueryProduct> _query;
+@override@JsonKey() List<QueryProduct> get query {
+  if (_query is EqualUnmodifiableListView) return _query;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_query);
+}
+
+
+@override
+String toString() {
+  return 'ListingCategoryState.error(message: $message, query: $query)';
+}
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _$ErrorImpl&&(identical(other.message, message) || other.message == message)&&const DeepCollectionEquality().equals(other._query, _query));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,message,const DeepCollectionEquality().hash(_query));
+
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+@pragma('vm:prefer-inline')
+_$$ErrorImplCopyWith<_$ErrorImpl> get copyWith => __$$ErrorImplCopyWithImpl<_$ErrorImpl>(this, _$identity);
+
+
+
+
+
+
+
+
+}
+
+
+abstract class Error implements ListingCategoryState {
+  const factory Error({required final  String message, final  List<QueryProduct> query}) = _$ErrorImpl;
+  
+
+  
+
+ String get message;@override List<QueryProduct> get query;
+/// Create a copy of ListingCategoryState
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+_$$ErrorImplCopyWith<_$ErrorImpl> get copyWith => throw _privateConstructorUsedError;
+
+}
