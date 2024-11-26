8075d3b179d397efa13ae4e55007ce42baa8cd08
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-27 04:43:22 +0800

    feat: add user search functionality to SearchResultState and SearchResultBloc

--- lib/dashboard/search/bloc/result/search_result_state.dart
+++ lib/dashboard/search/bloc/result/search_result_state.dart
@@ -5,18 +5,22 @@ class SearchResultState with _$SearchResultState {
   const factory SearchResultState.initial({
     @Default([]) List<Project> projects,
     @Default([]) List<ProjectCategory> categories,
+    @Default([]) List<User> users,
   }) = Initial;
   const factory SearchResultState.loading({
     @Default([]) List<Project> projects,
     @Default([]) List<ProjectCategory> categories,
+    @Default([]) List<User> users,
   }) = Loading;
   const factory SearchResultState.loaded({
     required List<Project> projects,
     required List<ProjectCategory> categories,
+    required List<User> users,
   }) = Loaded;
   const factory SearchResultState.error({
     @Default([]) List<Project> projects,
     @Default([]) List<ProjectCategory> categories,
+    @Default([]) List<User> users,
     required String message,
   }) = Error;
 }
