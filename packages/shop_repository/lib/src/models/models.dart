94739a427b1fda58e5de05ecaf8ac2ebb1b3deb3
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-27 00:38:38 +0800

    feat: update model exports to include review and query_review

--- packages/shop_repository/lib/src/models/models.dart
+++ packages/shop_repository/lib/src/models/models.dart
@@ -1,2 +1,4 @@
-export 'product.dart';
-export 'query_product.dart';
+export 'product/product.dart';
+export 'review/review.dart';
+export 'query_product/query_product.dart';
+export 'query_review.dart';
