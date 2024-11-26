459d86384bc0dfdfc16c502d425f21349b4a2438
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-27 04:45:16 +0800

    feat: add searchUsers method to ISearchRepository and implement in SearchRepository

--- packages/search_repository/lib/src/search_repository.dart
+++ packages/search_repository/lib/src/search_repository.dart
@@ -2,10 +2,12 @@ import 'package:config_repository/config_repository.dart';
 import 'package:project_repository/project_repository.dart';
 import 'package:search_repository/src/exceptions/search_exception.dart';
 import 'package:search_repository/src/models/search_suggestion/search_suggestion.dart';
+import 'package:user_repository/user_repository.dart';
 
 abstract class ISearchRepository {
   Future<List<SearchSuggestion>> searchSuggestions(String query);
   Future<List<Project>> searchProjects(String query);
+  Future<List<User>> searchUsers(String query);
 }
 
 class SearchRepository implements ISearchRepository {
@@ -60,6 +62,33 @@ class SearchRepository implements ISearchRepository {
           .toList();
 
       return suggestions;
+    } on RequestException catch (e) {
+      throw SearchException(e.message);
+    }
+  }
+
+  @override
+  Future<List<User>> searchUsers(String query) async {
+    try {
+      final response = await _config.makeRequest<Map<String, dynamic>>(
+        '/search/users',
+        method: 'GET',
+        queryParameters: <String, dynamic>{
+          'query': query,
+        },
+      );
+
+      if (response.data == null) {
+        throw const SearchException('No data found');
+      }
+
+      _config.logger.info('Response: ${response.data}');
+
+      final users = (response.data!['data'] as List<dynamic>)
+          .map((e) => User.fromJson(e))
+          .toList();
+
+      return users;
     } on RequestException catch (e) {
       throw SearchException(e.message);
     }
