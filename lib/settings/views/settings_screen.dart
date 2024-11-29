8d7b1a6f797b8534b8697bba146e3cea8b31b967
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-29 14:28:12 +0800

    feat: restore theme color selection functionality in SettingsScreen

--- lib/settings/views/settings_screen.dart
+++ lib/settings/views/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:craftmate_client/settings/bloc/settings_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -27,41 +28,41 @@ class SettingsScreen extends StatelessWidget {
                   },
                 ),
               ),
-              // ListTile(
-              //   title: const Text('Theme color'),
-              //   trailing: Container(
-              //     decoration: BoxDecoration(
-              //       shape: BoxShape.circle,
-              //       color: state.theme.colorScheme.primary,
-              //     ),
-              //     width: 24,
-              //     height: 24,
-              //   ),
-              //   onTap: () {
-              //     showDialog(
-              //       context: context,
-              //       builder: (BuildContext context) {
-              //         return AlertDialog(
-              //           titlePadding: EdgeInsets.zero,
-              //           contentPadding: EdgeInsets.zero,
-              //           content: SingleChildScrollView(
-              //             child: MaterialPicker(
-              //               pickerColor: state.theme.colorScheme.primary,
-              //               onColorChanged: (color) {
-              //                 context.read<SettingsBloc>().add(
-              //                       SettingsEvent.themeColorChanged(color),
-              //                     );
-              //                 Navigator.of(context).pop();
-              //               },
-              //               // enableLabel: _enableLabel,
-              //               // portraitOnly: _portraitOnly,
-              //             ),
-              //           ),
-              //         );
-              //       },
-              //     );
-              //   },
-              // ),
+              ListTile(
+                title: const Text('Theme color'),
+                trailing: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: state.theme.colorScheme.primary,
+                  ),
+                  width: 24,
+                  height: 24,
+                ),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        titlePadding: EdgeInsets.zero,
+                        contentPadding: EdgeInsets.zero,
+                        content: SingleChildScrollView(
+                          child: MaterialPicker(
+                            pickerColor: state.theme.colorScheme.primary,
+                            onColorChanged: (color) {
+                              context.read<SettingsBloc>().add(
+                                    SettingsEvent.themeColorChanged(color),
+                                  );
+                              Navigator.of(context).pop();
+                            },
+                            // enableLabel: _enableLabel,
+                            // portraitOnly: _portraitOnly,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ],
           );
         },
