0ff33b6d1c8ef3ed6d356ba7b47a48b633f2a196
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-30 23:26:20 +0800

    refactor: simplify SettingsPage by removing BlocProvider

--- lib/settings/views/settings_page.dart
+++ lib/settings/views/settings_page.dart
@@ -1,7 +1,5 @@
-import 'package:craftmate_client/settings/bloc/settings_bloc.dart';
 import 'package:craftmate_client/settings/views/settings_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -14,9 +12,6 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: context.read<SettingsBloc>(),
-      child: const SettingsScreen(),
-    );
+    return const SettingsScreen();
   }
 }
