98d6cf54d907deed0cd914d522190004bf268b93
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-30 23:24:54 +0800

    feat: integrate DashboardBloc for navigation handling in DashboardPage

--- lib/dashboard/view/dashboard_page.dart
+++ lib/dashboard/view/dashboard_page.dart
@@ -1,5 +1,10 @@
+import 'package:craftmate_client/dashboard/bloc/dashboard_bloc.dart';
 import 'package:craftmate_client/dashboard/home/bloc/home/home_bloc.dart';
+import 'package:craftmate_client/dashboard/shop/views/pages/view_listing_page.dart';
 import 'package:craftmate_client/dashboard/view/screens/dashboard_screen.dart';
+import 'package:craftmate_client/globals.dart';
+import 'package:craftmate_client/project_management/view_project/view/view_project_page.dart';
+import 'package:craftmate_client/user_profile/views/user_profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project_repository/project_repository.dart';
@@ -20,8 +25,25 @@ class DashboardPage extends StatelessWidget {
             projectRepo: context.read<ProjectRepository>(),
           ),
         ),
+        BlocProvider(
+          create: (context) => DashboardBloc(
+            appLinks: config.appLinks,
+          ),
+        ),
       ],
-      child: const DashboardScreen(),
+      child: BlocListener<DashboardBloc, DashboardState>(
+        listener: (context, state) {
+          switch (state) {
+            case ProjectReceived(:final id):
+              Navigator.push(context, ViewProjectPage.route(id));
+            case ShopReceived(:final id):
+              Navigator.push(context, ViewListingPage.route(id));
+            case UserReceived(:final id):
+              Navigator.push(context, UserProfilePage.route(id));
+          }
+        },
+        child: const DashboardScreen(),
+      ),
     );
   }
 }
