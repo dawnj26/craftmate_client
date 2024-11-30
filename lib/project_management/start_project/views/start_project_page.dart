a414bf6ef5f6695205d816a0f80f4f4428e873d4
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-30 23:25:41 +0800

    feat: integrate showcase tutorial with scroll functionality in StartProjectPage

--- lib/project_management/start_project/views/start_project_page.dart
+++ lib/project_management/start_project/views/start_project_page.dart
@@ -1,7 +1,10 @@
+import 'package:craftmate_client/globals.dart';
 import 'package:craftmate_client/project_management/start_project/bloc/start/start_project_bloc.dart';
 import 'package:craftmate_client/project_management/start_project/views/screens/start_project_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:showcaseview/showcaseview.dart';
 
 class StartProjectPage extends StatelessWidget {
   const StartProjectPage({super.key, required this.id});
@@ -18,12 +21,33 @@ class StartProjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => StartProjectBloc(
-        projectRepository: context.read(),
-        projectId: id,
-      )..add(const StartProjectEvent.started()),
-      child: const StartProjectScreen(),
+    final scrollController = ItemScrollController();
+
+    return ShowCaseWidget(
+      onStart: (index, key) {
+        if (index == 3) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            scrollController.jumpTo(
+              index: 3,
+              alignment: 0.2,
+            );
+          });
+        }
+      },
+      onFinish: () {
+        config.prefs.setBool('hasSeenTrackerTutorial', true);
+      },
+      builder: (context) {
+        return BlocProvider(
+          create: (context) => StartProjectBloc(
+            projectRepository: context.read(),
+            projectId: id,
+          )..add(const StartProjectEvent.started()),
+          child: StartProjectScreen(
+            scrollController: scrollController,
+          ),
+        );
+      },
     );
   }
 }
