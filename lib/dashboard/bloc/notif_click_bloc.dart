6c3078ec2470662b71483e396824a0432edbc139
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-12-02 15:29:10 +0800

    feat: enhance NotifClickBloc to include userId and projectId, and integrate ShopRepository for product queries

--- lib/dashboard/bloc/notif_click_bloc.dart
+++ lib/dashboard/bloc/notif_click_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:notification_repository/notification_repository.dart';
 import 'package:project_repository/project_repository.dart';
+import 'package:shop_repository/shop_repository.dart';
 import 'package:user_repository/user_repository.dart';
 import 'package:uuid/uuid.dart';
 
@@ -14,6 +15,7 @@ class NotifClickBloc extends Bloc<NotifClickEvent, NotifClickState> {
     this._notificationRepository,
     this._userRepository,
     this._projectRepository,
+    this._shopRepository,
   ) : super(const Initial()) {
     on<_NotificationClicked>(_onNotificationClicked);
   }
@@ -21,6 +23,7 @@ class NotifClickBloc extends Bloc<NotifClickEvent, NotifClickState> {
   final NotificationRepository _notificationRepository;
   final UserRepository _userRepository;
   final ProjectRepository _projectRepository;
+  final ShopRepository _shopRepository;
 
   Future<void> _onNotificationClicked(
     _NotificationClicked event,
@@ -37,11 +40,43 @@ class NotifClickBloc extends Bloc<NotifClickEvent, NotifClickState> {
             return;
           }
           final user = await _userRepository.getUserById(userId);
+          await _notificationRepository.markNotificationAsRead(
+            event.userId,
+            event.notification.id,
+          );
+          final queryProduct = event.notification.data['listingId'] != null
+              ? await _shopRepository.fetchListing(
+                  event.notification.data['listingId'] as String,
+                  event.userId,
+                )
+              : null;
+
           emit(
             ChatClicked(
               uuid: const Uuid().v4(),
               notification: event.notification,
               user: user,
+              queryProduct: queryProduct,
+            ),
+          );
+        case 'project':
+          final projectId = event.notification.data['id'] as int?;
+
+          if (projectId == null) {
+            emit(const Error(error: 'Failed to load notification'));
+            return;
+          }
+
+          await _notificationRepository.markNotificationAsRead(
+            event.userId,
+            event.notification.id,
+          );
+
+          emit(
+            ProjectClicked(
+              uuid: const Uuid().v4(),
+              notification: event.notification,
+              projectId: projectId,
             ),
           );
       }
--- lib/dashboard/bloc/notif_click_event.dart
+++ lib/dashboard/bloc/notif_click_event.dart
@@ -4,5 +4,6 @@ part of 'notif_click_bloc.dart';
 class NotifClickEvent with _$NotifClickEvent {
   const factory NotifClickEvent.notificationClicked({
     required CNotification notification,
+    required int userId,
   }) = _NotificationClicked;
 }
