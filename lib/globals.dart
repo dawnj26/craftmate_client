bef04b882f4da0a222f12b385943867db302e821
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-12-02 14:06:59 +0800

    feat: add Flutter local notifications plugin and notification channel variables in globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,11 @@
 import 'package:config_repository/config_repository.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 import 'package:log_collector/log_collector.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 late final LogCollector logger;
 late final ConfigRepository config;
 late final SharedPreferences prefs;
+late final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+late final AndroidNotificationChannel channel;
