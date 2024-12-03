f73b123aba882fa1d8de6456069036c39d508544
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-12-04 00:13:11 +0800

    feat: create report_repository.dart to support library exports and documentation

---
+++ packages/report_repository/lib/report_repository.dart
@@ -0,0 +1,10 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/report_repository_base.dart';
+export 'src/models/models.dart';
+export 'src/exceptions/exceptions.dart';
+
+// TODO: Export any libraries intended for clients of this package.
